7fc9f949e7d7362468bcaae443c6e9a98fc94bc9
Kader SAHNINE <a.sahnine@netcourrier.com>
2022-04-29 15:08:27 +0200

    Add RTSP project

---
+++ RabbitOM.Net.Rtps/RTSPDateTimeFormatType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RabbitOM.Net.Rtsp
+{
+    /// <summary>
+    /// Represent the datetime format types
+    /// </summary>
+    public static class RTSPDateTimeFormatType
+    {
+        /// <summary>
+        /// Represent a greenwich mean time format
+        /// </summary>
+        public const string GmtFormat  = "ddd, dd MMM yyyy HH:mm:ss Z";
+
+        /// <summary>
+        /// Represent a greenwich mean time format
+        /// </summary>
+        public const string GmtFormat1 = "ddd, MMM dd yyyy HH:mm:ss Z";
+
+        /// <summary>
+        /// Represent a greenwich mean time format
+        /// </summary>
+        public const string GmtFormat2 = "ddd, yyyy MMM dd HH:mm:ss Z";
+
+        /// <summary>
+        /// Represent a greenwich mean time format
+        /// </summary>
+        public const string GmtFormat3 = "ddd, yyyy dd MMM HH:mm:ss Z";
+    }
+}
---
+++ RabbitOM.Net.Rtps/RTSPHashAlgorithm.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace RabbitOM.Net.Rtsp
+{
+    /// <summary>
+    /// Represent a class used to generate a digest value using MD5 algorithm
+    /// </summary>
+    public static class RTSPHashAlgorithm
+    {
+        /// <summary>
+        /// Create a hash
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>returns a string</returns>
+        public static string ComputeMD5Hash( string input )
+        {
+            return ComputeHash( MD5.Create() , input );
+        }
+
+        /// <summary>
+        /// Create a hash
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>returns a string</returns>
+        public static string ComputeSHA1Hash( string input )
+        {
+            return ComputeHash( SHA1.Create() , input );
+        }
+
+        /// <summary>
+        /// Create a hash
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>returns a string</returns>
+        public static string ComputeSHA256Hash( string input )
+        {
+            return ComputeHash( SHA256.Create() , input );
+        }
+
+        /// <summary>
+        /// Create a hash
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>returns a string</returns>
+        public static string ComputeSHA512Hash( string input )
+        {
+            return ComputeHash( SHA512.Create() , input );
+        }
+
+        /// <summary>
+        /// Create a hash
+        /// </summary>
+        /// <param name="algorithm">the algorithm</param>
+        /// <param name="input"></param>
+        /// <returns>returns a string</returns>
+        private static string ComputeHash( HashAlgorithm algorithm , string input )
+        {
+            if ( algorithm == null )
+            {
+                return string.Empty;
+            }
+
+            using ( algorithm )
+            {
+                if ( string.IsNullOrEmpty( input ) )
+                {
+                    return string.Empty;
+                }
+
+                var plainBytes = System.Text.Encoding.ASCII.GetBytes( input );
+
+                if ( plainBytes == null || plainBytes.Length <= 0 )
+                {
+                    return string.Empty;
+                }
+
+                var digestBytes = algorithm.ComputeHash( plainBytes );
+
+                if ( digestBytes == null || digestBytes.Length <= 0 )
+                {
+                    return string.Empty;
+                }
+
+                var output = new StringBuilder();
+
+                for ( int i = 0 ; i < digestBytes.Length ; i++ )
+                {
+                    output.Append( digestBytes[i].ToString( "x2" ) );
+                }
+
+                return output.ToString();
+            }
+        }
+    }
+}
