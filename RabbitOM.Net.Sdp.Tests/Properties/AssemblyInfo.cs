c396c9b21750a952f1699c49817fe9e78ff8eea1
Kader SAHNINE <a.sahnine@netcourrier.com>
2023-11-06 13:09:41 +0100

    Changing version

--- RabbitOM.Net.Sdp.Tests/Properties/AssemblyInfo.cs
+++ RabbitOM.Net.Sdp.Tests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b7656250-fa9e-40df-a52d-2cde04680940")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
