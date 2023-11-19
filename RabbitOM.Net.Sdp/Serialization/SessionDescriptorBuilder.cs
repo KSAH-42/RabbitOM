a1c433abab3c3a2f2f85cd603cd61569d6687002
Kader SAHNINE <a.sahnine@netcourrier.com>
2023-11-19 17:55:51 +0100

    Seal the builder class, and remove the fluent feature, it was never used

--- RabbitOM.Net.Sdp/Serialization/SessionDescriptorBuilder.cs
+++ RabbitOM.Net.Sdp/Serialization/SessionDescriptorBuilder.cs
@@ -3,7 +3,7 @@
 	/// <summary>
 	/// Represent a session description builder
 	/// </summary>
-	public class SessionDescriptorBuilder
+	public sealed class SessionDescriptorBuilder
 	{
 		private VersionField _version = null;
 
@@ -44,261 +44,204 @@
 		/// Set the version
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetVersion(string headerValue)
+		public void SetVersion(string headerValue)
 		{
 			VersionField.TryParse(headerValue, out _version);
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set the origin
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetOrigin(string headerValue)
+		public void SetOrigin(string headerValue)
 		{
 			OriginField.TryParse(headerValue, out _origin);
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set the session name
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetSessionName(string headerValue)
+		public void SetSessionName(string headerValue)
 		{
 			SessionNameField.TryParse(headerValue, out _sessionName);
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set the session information
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetSessionInformation(string headerValue)
+		public void SetSessionInformation(string headerValue)
 		{
 			SessionInformationField.TryParse(headerValue, out _sessionInformation);
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set the uri
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetUri(string headerValue)
+		public void SetUri(string headerValue)
 		{
 			UriField.TryParse(headerValue, out _uri);
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set the connection
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetConnection(string headerValue)
+		public void SetConnection(string headerValue)
 		{
 			ConnectionField.TryParse(headerValue, out _connection);
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set the time zone
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetTimeZone(string headerValue)
+		public void SetTimeZone(string headerValue)
 		{
 			TimeZoneField.TryParse(headerValue, out _timeZone);
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set the encryption key
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetEncryption(string headerValue)
+		public void SetEncryption(string headerValue)
 		{
 			EncryptionField.TryParse(headerValue, out _encryption);
-
-			return this;
 		}
 
 		/// <summary>
 		/// Add the email
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder AddEmail(string headerValue)
+		public void AddEmail(string headerValue)
 		{
 			if (EmailField.TryParse(headerValue, out EmailField field))
 			{
 				_emails.TryAdd(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Add a phone number
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder AddPhone(string headerValue)
+		public void AddPhone(string headerValue)
 		{
 			if (PhoneField.TryParse(headerValue, out PhoneField field))
 			{
 				_phones.TryAdd(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Add a bandwith
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder AddBandwith(string headerValue)
+		public void AddBandwith(string headerValue)
 		{
 			if (BandwithField.TryParse(headerValue, out BandwithField field))
 			{
 				_bandwiths.TryAdd(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Add a time values
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder AddTime(string headerValue)
+		public void AddTime(string headerValue)
 		{
 			if (TimeField.TryParse(headerValue, out TimeField field))
 			{
 				_times.TryAdd(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Add a repeat time values
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder AddRepeat(string headerValue)
+		public void AddRepeat(string headerValue)
 		{
 			if (RepeatField.TryParse(headerValue, out RepeatField field))
 			{
 				_repeats.TryAdd(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Add an attribute
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder AddAttribute(string headerValue)
+		public void AddAttribute(string headerValue)
 		{
 			if (AttributeField.TryParse(headerValue, out AttributeField field))
 			{
 				_attributes.TryAdd(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Create a media description
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder CreateMediaDescription(string headerValue)
+		public void CreateMediaDescription(string headerValue)
 		{
 			if (MediaDescriptionField.TryParse(headerValue, out _currentMediaDescription))
 			{
 				_mediaDescriptions.TryAdd(_currentMediaDescription);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set a connection on the current media description
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetMediaConnection(string headerValue)
+		public void SetMediaConnection(string headerValue)
 		{
 			if (_currentMediaDescription != null && ConnectionField.TryParse(headerValue, out ConnectionField field))
 			{
 				_currentMediaDescription.Connection.CopyFrom(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Set a encryption on the current media description
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder SetMediaEncryption(string headerValue)
+		public void SetMediaEncryption(string headerValue)
 		{
 			if (_currentMediaDescription != null && EncryptionField.TryParse(headerValue, out EncryptionField field))
 			{
 				_currentMediaDescription.Encryption.CopyFrom(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Add a bandwith on the current media description
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder AddMediaBandwith(string headerValue)
+		public void AddMediaBandwith(string headerValue)
 		{
 			if (_currentMediaDescription != null && BandwithField.TryParse(headerValue, out BandwithField field))
 			{
 				_currentMediaDescription.Bandwiths.TryAdd(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
 		/// Add an attribute on the current media description
 		/// </summary>
 		/// <param name="headerValue">the header value</param>
-		/// <returns>returns the builder instance</returns>
-		public SessionDescriptorBuilder AddMediaAttribute(string headerValue)
+		public void AddMediaAttribute(string headerValue)
 		{
 			if (_currentMediaDescription != null && AttributeField.TryParse(headerValue, out AttributeField field))
 			{
 				_currentMediaDescription.Attributes.TryAdd(field);
 			}
-
-			return this;
 		}
 
 		/// <summary>
