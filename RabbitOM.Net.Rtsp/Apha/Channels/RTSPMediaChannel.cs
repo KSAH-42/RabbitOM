a27a5aec8e35d610f7dccf6a78e69552fb0bc4bb
Kader SAHNINE <a.sahnine@netcourrier.com>
2023-12-13 17:42:47 +0100

    Add RTSPMediaChannel class

---
+++ RabbitOM.Net.Rtsp/Apha/Channels/RTSPMediaChannel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace RabbitOM.Net.Rtsp.Apha
+{
+    public sealed class RTSPMediaChannel : IRTSPMediaChannel
+    {
+        private readonly IRTSPClientDispatcher _dispatcher;
+
+
+
+
+
+        public RTSPMediaChannel(IRTSPClientDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher ?? throw new ArgumentNullException( nameof( dispatcher ) );
+        }
+
+
+
+
+
+        public object SyncRoot
+            => throw new NotImplementedException();
+        
+        public IRTSPClientConfiguration Configuration
+            => throw new NotImplementedException();
+        
+        public IRTSPClientDispatcher Dispatcher
+            => throw new NotImplementedException();
+        
+        public bool IsConnected
+            => throw new NotImplementedException();
+        
+        public bool IsReceivingPacket
+            => throw new NotImplementedException();
+        
+        public bool IsStreamingStarted
+            => throw new NotImplementedException();
+        
+        public bool IsDisposed
+            => throw new NotImplementedException();
+
+
+
+
+
+        public bool Connect()
+            => throw new NotImplementedException();
+        
+        public bool Disconnect()
+            => throw new NotImplementedException();
+        
+        public void Dispose()
+            => throw new NotImplementedException();
+        
+        public void Abort()
+            => throw new NotImplementedException();
+        
+        public bool StartStreaming()
+            => throw new NotImplementedException();
+        
+        public bool StopStreaming()
+            => throw new NotImplementedException();
+        
+        public bool KeepAlive()
+            => throw new NotImplementedException();
+        
+        public bool WaitForConnection(TimeSpan shutdownTimeout)
+            => throw new NotImplementedException();
+
+
+
+
+
+        private void OnConnected( RTSPConnectedEventArgs e )
+        {
+            _dispatcher.Dispatch( e );
+        }
+
+        private void OnDisconnected( RTSPDisconnectedEventArgs e )
+        {
+            _dispatcher.Dispatch( e );
+        }
+
+        private void OnMessageReceived( RTSPMessageReceivedEventArgs e )
+        {
+            _dispatcher.Dispatch( e );
+        }
+
+        private void OnError( RTSPErrorEventArgs e )
+        {
+            _dispatcher.Dispatch( e );
+        }
+    }
+}
