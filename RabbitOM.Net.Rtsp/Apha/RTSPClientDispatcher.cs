231fef17f9bdc08693728e40aca0d98300dc4b33
Kader SAHNINE <a.sahnine@netcourrier.com>
2023-12-13 16:58:03 +0100

    Implement the RTSPClientDispatcher class

---
+++ RabbitOM.Net.Rtsp/Apha/RTSPClientDispatcher.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace RabbitOM.Net.Rtsp.Apha
+{
+    public sealed class RTSPClientDispatcher : IRTSPClientDispatcher, IDisposable
+    {
+        private readonly RTSPEventQueue _queue;
+
+        private readonly RTSPThread _thread;
+
+        private readonly Action<EventArgs> _handler;
+
+
+
+
+
+        public RTSPClientDispatcher( Action<EventArgs> handler )
+        {
+            _handler = handler ?? throw new ArgumentNullException( nameof( handler ) );
+
+            _queue = new RTSPEventQueue();
+            _thread = new RTSPThread("RTSP - Dispatcher thread");
+        }
+
+
+
+
+
+        public void Start()
+        {
+            _thread.Start( DoEvents ); 
+        }
+
+        public void Stop()
+        {
+            _thread.Stop();
+            _queue.Clear();
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public void Dispatch(EventArgs e)
+        {
+            _queue.Enqueue(e);
+        }
+
+        public void RaiseEvent(EventArgs e)
+        {
+            _handler.Invoke(e);
+        }
+
+
+
+
+
+        private void DoEvents()
+        {
+            var pumpEvents = new Action( () =>
+            {
+                while ( _queue.TryDequeue( out EventArgs e ) )
+                {
+                    _handler.Invoke( e );
+                }
+            });
+
+            while ( RTSPEventQueue.Wait( _queue , _thread.ExitHandle ) )
+            {
+                pumpEvents();
+            }
+
+            pumpEvents();
+        }
+    }
+}
