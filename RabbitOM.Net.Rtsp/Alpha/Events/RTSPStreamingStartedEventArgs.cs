3fe6fdb180406121ef18b4ddeec9c341dbe33927
Kader SAHNINE <a.sahnine@netcourrier.com>
2023-12-16 23:56:53 +0100

    Remove the TrackInfo property

--- RabbitOM.Net.Rtsp/Alpha/Events/RTSPStreamingStartedEventArgs.cs
+++ RabbitOM.Net.Rtsp/Alpha/Events/RTSPStreamingStartedEventArgs.cs
@@ -4,16 +4,5 @@ namespace RabbitOM.Net.Rtsp.Alpha
 {
     public class RTSPStreamingStartedEventArgs : EventArgs
     {
-        private readonly RTSPTrackInfo _trackInfo;
-
-        public RTSPStreamingStartedEventArgs( RTSPTrackInfo trackInfo )
-        {
-            _trackInfo = trackInfo ?? throw new ArgumentNullException( nameof( trackInfo ) );
-        }
-
-        public RTSPTrackInfo TrackInfo
-        {
-            get => _trackInfo;
-        }
     }
 }
