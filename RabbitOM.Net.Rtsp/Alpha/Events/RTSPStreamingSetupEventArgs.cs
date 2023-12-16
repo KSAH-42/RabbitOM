ae3aafc6129c955dc56e5d4c1d2e3abbd625aece
Kader SAHNINE <a.sahnine@netcourrier.com>
2023-12-16 23:58:22 +0100

    Add an eventarg which contains the trackinfor, in order to alert a successfull setup

---
+++ RabbitOM.Net.Rtsp/Alpha/Events/RTSPStreamingSetupEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RabbitOM.Net.Rtsp.Alpha
+{
+    public class RTSPStreamingSetupEventArgs : EventArgs
+    {
+        private readonly RTSPTrackInfo _trackInfo;
+
+        public RTSPStreamingSetupEventArgs( RTSPTrackInfo trackInfo )
+        {
+            _trackInfo = trackInfo ?? throw new ArgumentNullException( nameof( trackInfo ) );
+        }
+
+        public RTSPTrackInfo TrackInfo
+        {
+            get => _trackInfo;
+        }
+    }
+}
