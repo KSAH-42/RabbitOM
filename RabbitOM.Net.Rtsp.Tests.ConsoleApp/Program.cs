26b84707c379751eb7db16e3475e9ab5701ac1e7
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-02-27 07:12:05 +0100

    refactor the main

--- RabbitOM.Net.Rtsp.Tests.ConsoleApp/Program.cs
+++ RabbitOM.Net.Rtsp.Tests.ConsoleApp/Program.cs
@@ -23,14 +23,9 @@ namespace RabbitOM.Net.Rtsp.Tests.ConsoleApp
                 return;
             }
 
-            Action<string> test = Program.Run;
-
-            if ( commandLines.SinkOption )
-                test = Program.RunWithSink;
-
             try
             {
-                test( commandLines.UriOption );
+                Run( commandLines );
             }
             finally
             {
@@ -46,9 +41,9 @@ namespace RabbitOM.Net.Rtsp.Tests.ConsoleApp
         // I strongly recommend to BUY a camera, it is better, and don't waste your time to find a security camera online from any manufacturers
         // Otherwise you can use HappyRtspServer software but it does not reflect an ip security camera
 
-        static void Run(string uri)
+        static void Run( CommandLines commandLines )
         {         
-            if ( ! RTSPUri.TryParse( uri , out RTSPUri rtspUri) )
+            if ( ! RTSPUri.TryParse( commandLines.UriOption , out RTSPUri rtspUri) )
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Bad uri");
@@ -56,6 +51,7 @@ namespace RabbitOM.Net.Rtsp.Tests.ConsoleApp
             }
 
             using ( var client = new RTSPClient() )
+            using ( var rtpSink= new DefaultRTPSink() )
             {
                 client.CommunicationStarted += ( sender , e ) =>
                 {
@@ -99,15 +95,34 @@ namespace RabbitOM.Net.Rtsp.Tests.ConsoleApp
                         return;
                     }
 
-                    if ( ! RtpPacket.TryParse( e.Packet.Data , out RtpPacket packet ) )
+                    if ( ! commandLines.SinkOption )
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine( "Invalid rtp packet !!!!" );
-                        return;
+                        if ( !RTPPacket.TryParse( e.Packet.Data , out RTPPacket packet ) )
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine( "Invalid rtp packet !!!!" );
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        rtpSink.Write( e.Packet.Data );
                     }
 
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine( "DataReceived {0}" , packet.Data.Count );
+                    Console.WriteLine( "DataReceived {0}" , e.Packet.Data.Length );
+                };
+
+                rtpSink.PacketReceived += ( sender , e ) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine( "Rtp packet received : {0}" , e.Packet.Data.Length );
+                };
+
+                rtpSink.FrameReceived += ( sender , e ) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine( "New Rtp frame - packet_count : {0} " , e.Frame.Packets.Length );
                 };
 
                 // Please note, read the manufacturer's documentation
@@ -130,107 +145,7 @@ namespace RabbitOM.Net.Rtsp.Tests.ConsoleApp
 
                 // client.Configuration.MulticastAddress = "229.0.0.1";
                 // client.Configuration.RtpPort = 55000;
-                client.StartCommunication();
 
-                Console.CancelKeyPress += ( sender , e ) => Console.ForegroundColor = ConsoleColor.White;
-
-                Console.WriteLine( "Press any keys to close the application" );
-                Console.ReadKey();
-
-                client.StopCommunication( TimeSpan.FromSeconds( 3 ) );
-            }
-        }
-
-        static void RunWithSink( string uri )
-        {
-            if ( !RTSPUri.TryParse( uri , out RTSPUri rtspUri ) )
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine( "Bad uri" );
-                return;
-            }
-
-            using ( var client = new RTSPClient() )
-            using ( var rtpSink = new DefaultRTPSink() )
-            {
-                client.CommunicationStarted += ( sender , e ) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine( "Communication started - " + DateTime.Now );
-                };
-
-                client.CommunicationStopped += ( sender , e ) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine( "Communication stopped - " + DateTime.Now );
-                };
-
-                client.Connected += ( sender , e ) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine( "Client connected - " + client.Configuration.Uri );
-                    rtpSink.Reset();
-                };
-
-                client.Disconnected += ( sender , e ) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine( "Client disconnected - " + DateTime.Now );
-                };
-
-                client.Error += ( sender , e ) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine( "Client Error: " + e.Code );
-                };
-
-                client.PacketReceived += ( sender , e ) =>
-                {
-                    var interleavedPacket = e.Packet as RTSPInterleavedPacket;
-
-                    if ( interleavedPacket != null && interleavedPacket.Channel > 0 )
-                    {
-                        // In most of case, avoid this packet
-                        Console.ForegroundColor = ConsoleColor.DarkCyan;
-                        Console.WriteLine( "Skipping some data : size {0}" , e.Packet.Data.Length );
-                        return;
-                    }
-
-                    rtpSink.Write( e.Packet.Data );
-                };
-
-                rtpSink.PacketReceived += ( sender , e ) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine( "Rtp packet received : {0}" , e.Packet.Data.Length );
-                };
-
-                rtpSink.FrameReceived += ( sender , e ) =>
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.WriteLine( "New Rtp frame - packet_count : {0} " , e.Frame.Packets.Length );
-                };
-
-                // Please note, read the manufacturer's documentation
-                // to get the right uri
-
-                client.Configuration.Uri = rtspUri.ToString( true );
-                client.Configuration.UserName = rtspUri.UserName;
-                client.Configuration.Password = rtspUri.Password;
-                client.Configuration.ReceiveTimeout = TimeSpan.FromSeconds( 3 );
-                client.Configuration.SendTimeout = TimeSpan.FromSeconds( 3 );
-                client.Configuration.KeepAliveType = RTSPKeepAliveType.Options;
-                client.Configuration.MediaFormat = RTSPMediaFormat.Video;
-                client.Configuration.DeliveryMode = RTSPDeliveryMode.Tcp;
-
-                // For multicast settings, please make sure
-                // that the camera or the video source support multicast
-                // For instance, the happy RTSP server does not support multicast
-                // AND make sure that your are used a switch not a hub, very is difference between them
-                // And activate igmp snooping on the switch
-
-                // client.Configuration.MulticastAddress = "229.0.0.1";
-                // client.Configuration.RtpPort = 55000;
                 client.StartCommunication();
 
                 Console.CancelKeyPress += ( sender , e ) => Console.ForegroundColor = ConsoleColor.White;
