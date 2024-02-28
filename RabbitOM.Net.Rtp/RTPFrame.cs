55f0e3fb949063976f9ddf4a7164bcebefc330bc
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-02-28 16:27:07 +0100

    small refactoring of frame class

--- RabbitOM.Net.Rtp/RTPFrame.cs
+++ RabbitOM.Net.Rtp/RTPFrame.cs
@@ -8,12 +8,35 @@ namespace RabbitOM.Net.Rtp
 {
     public sealed class RTPFrame
     {
+        private readonly RTPPacket[] _packets;
+
+
+
         public RTPFrame( RTPPacket[] packets ) 
-            => Packets = packets;
-        public RTPPacket[] Packets { get; private set; }
+        {
+            if ( packets == null )
+                throw new ArgumentNullException( nameof( packets ) );
+
+            if ( packets.Length == 0 )
+                throw new ArgumentException( nameof( packets ) );
+
+            _packets = packets;
+        }
+
+
+
+
+        public RTPPacket[] Packets 
+        {
+            get => _packets;
+        }
+
+
+
 
         public bool TryValidate()
-            => Packets == null || Packets.Length == 0 ? false : true
-            ;
+        {
+            return _packets == null || _packets.Length == 0 ? false : true;
+        }
     }
 }
