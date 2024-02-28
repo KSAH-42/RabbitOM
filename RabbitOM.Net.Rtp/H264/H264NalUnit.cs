f6adacc47f7c2fa8e769b8d17b4e9ae8334add8d
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-02-28 16:22:02 +0100

    remove some comments

--- RabbitOM.Net.Rtp/H264/H264NalUnit.cs
+++ RabbitOM.Net.Rtp/H264/H264NalUnit.cs
@@ -3,8 +3,7 @@
 
               O P T I M I Z A T I O N S
 
- For next implementation, optimize can to reduce copy 
- by using array segment to remove Buffer.Copy or similar methods
+ Reduce copy by using array segment to remove Buffer.Copy or similar methods
 
 */
 
--- RabbitOM.Net.Rtp/H264/H264NalUnitCollection.cs
+++ RabbitOM.Net.Rtp/H264/H264NalUnitCollection.cs
@@ -1,11 +1,5 @@
 ﻿/*
  EXPERIMENTATION of the next implementation of the rtp layer
-
-              O P T I M I Z A T I O N S
-
- For next implementation, optimize can to reduce copy 
- by using array segment to remove Buffer.Copy or similar methods
-
 */
 
 using System;
