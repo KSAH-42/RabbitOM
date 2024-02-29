4ca64ae308ec906f193d1bd00028c8c605d514eb
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-02-29 19:24:15 +0100

    Continue the rtp layer implementation

--- RabbitOM.Net.Rtp/StartPrefix.cs
+++ RabbitOM.Net.Rtp/StartPrefix.cs
@@ -63,5 +63,32 @@ namespace RabbitOM.Net.Rtp
 
             return true;
         }
+
+        public static int IndexOfPrefix( byte[] buffer )
+        {
+            return IndexOfPrefix( buffer , 3 );
+        }
+
+        public static int IndexOfPrefix( byte[] buffer , int count /*the prefix size*/ ) 
+        {
+            if ( null == buffer )
+                return -1;
+
+            int numberOfZeros = 0;
+
+            for ( int i = 0 ; i < buffer.Length && i <= count ; ++ i )
+            {
+                if ( buffer[ i ] > 1 )
+                    break;
+
+                if ( buffer[ i ] == 0 )
+                    numberOfZeros ++;
+
+                if ( buffer[ i ] == 1 )
+                    return numberOfZeros >= 2 ? i : -1;
+            }
+
+            return -1;
+        }
     }
 }
