dc14c866ff7eb34760b7f5f08548f0118ee674a6
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-13 20:29:22 +0200

    add comparision methods for bytes array

---
+++ RabbitOM.Streaming/ArrayExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RabbitOM.Streaming
+{
+    /// <summary>
+    /// Represent an array extensions class
+    /// </summary>
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Byte array comparision
+        /// </summary>
+        /// <param name="source">the source</param>
+        /// <param name="target">the target</param>
+        /// <returns>returns true for a success, otherwise false</returns>
+        public static bool IsEquals( this byte[] source , byte[] target )
+        {
+            if ( object.ReferenceEquals( source , target ) )
+            {
+                return true;
+            }
+
+            if ( object.ReferenceEquals( source , null ) || object.ReferenceEquals( target , null ) )
+            {
+                return false;
+            }
+
+            if ( source.Length != target.Length )
+            {
+                return false;
+            }
+
+            for ( int i = 0 ; i < source.Length ; ++ i )
+            {
+                if ( source[ i ] != target[ i ] )
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
