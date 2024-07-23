a81d633e74979fcf95a684cdad83808f6aa3988f
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-23 17:32:22 +0200

    adding methods

--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegQuantizationTableFactory.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegQuantizationTableFactory.cs
@@ -3,10 +3,29 @@
 namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 {
     public sealed class JpegQuantizationTableFactory
-    {
+    {                                               
+        private ArraySegment<byte> _table = default;
+
+
+
+
+        public ArraySegment<byte> Table
+        {
+            get => _table;
+            set => _table = value;
+        }
+
+
+
+
         public int GetNumberOfTables()
         {
             throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            _table = default;
         }
     }
 }
