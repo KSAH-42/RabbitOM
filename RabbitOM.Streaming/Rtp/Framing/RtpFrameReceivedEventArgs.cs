d33c7fae3b198cc9d8e13d12007dbee9e47525af
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-28 21:10:23 +0200

    add comments

--- RabbitOM.Streaming/Rtp/Framing/RtpFrameReceivedEventArgs.cs
+++ RabbitOM.Streaming/Rtp/Framing/RtpFrameReceivedEventArgs.cs
@@ -2,15 +2,26 @@
 
 namespace RabbitOM.Streaming.Rtp.Framing
 {
+    /// <summary>
+    /// Represent the frame event args
+    /// </summary>
     public class RtpFrameReceivedEventArgs : EventArgs
     {
         private readonly RtpFrame _frame;
 
+        /// <summary>
+        /// Initialize a new instancel of the frame event args
+        /// </summary>
+        /// <param name="frame">the frame</param>
+        /// <exception cref="ArgumentNullException"/>
         public RtpFrameReceivedEventArgs( RtpFrame frame )
         {
-            _frame = frame ?? throw new ArgumentNullException( nameof( frame ) ); ;
+            _frame = frame ?? throw new ArgumentNullException( nameof( frame ) );
         }
 
+        /// <summary>
+        /// Gets the frame
+        /// </summary>
         public RtpFrame Frame
         {
             get => _frame;
