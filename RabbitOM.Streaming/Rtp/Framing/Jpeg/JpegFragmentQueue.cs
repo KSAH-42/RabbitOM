b67dff1901cca7515127015258f17c5595c86316
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-29 00:10:54 +0200

    add comments

--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegFragmentQueue.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegFragmentQueue.cs
@@ -4,24 +4,40 @@ using System.Collections.Generic;
 
 namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 {
+    /// <summary>
+    /// Represent a the fragment queue collection
+    /// </summary>
     public sealed class JpegFragmentQueue : IEnumerable , IEnumerable<JpegFragment> , IReadOnlyCollection<JpegFragment>
     {
         private readonly Queue<JpegFragment> _collection;
 
 
 
-
+        /// <summary>
+        /// Initialize a new instance of the fragment queue
+        /// </summary>
 
         public JpegFragmentQueue()
         {
             _collection = new Queue<JpegFragment>();
         }
 
+        /// <summary>
+        /// Initialize a new instance of the fragment queue
+        /// </summary>
+        /// <param name="capacity">the capacity</param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
         public JpegFragmentQueue( int capacity )
         {
             _collection = new Queue<JpegFragment>( capacity );
         }
 
+
+        /// <summary>
+        /// Initialize a new instance of the fragment queue
+        /// </summary>
+        /// <param name="collection">the collection</param>
+        /// <exception cref="ArgumentNullException"/>
         public JpegFragmentQueue( IEnumerable<JpegFragment> collection )
         {
             if ( collection == null )
@@ -39,14 +55,17 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 
 
 
-
-
-
+        /// <summary>
+        /// Gets the number of elements
+        /// </summary>
         public int Count
         {
             get => _collection.Count;
         }
 
+        /// <summary>
+        /// Check if the collection empty
+        /// </summary>
         public bool IsEmpty
         {
             get => _collection.Count == 0;
@@ -54,55 +73,95 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 
 
 
-
-
-
-
+        /// <summary>
+        /// Gets an enumerator
+        /// </summary>
+        /// <returns>returns an instance</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             return _collection.GetEnumerator();
         }
 
+        /// <summary>
+        /// Gets an enumerator
+        /// </summary>
+        /// <returns>returns an instance</returns>
         public IEnumerator<JpegFragment> GetEnumerator()
         {
             return _collection.GetEnumerator();
         }
 
+        /// <summary>
+        /// Check if the collection has elements
+        /// </summary>
+        /// <returns>returns true for a success, otherwise false</returns>
         public bool Any()
         {
             return _collection.Count > 0;
         }
 
+        /// <summary>
+        /// Check if the collection contains a particular elements
+        /// </summary>
+        /// <param name="fragment">the fragment</param>
+        /// <returns>returns true for a success, otherwise false</returns>
         public bool Contains( JpegFragment fragment )
         {
             return _collection.Contains( fragment );
         }
 
+        /// <summary>
+        /// Clear the collection
+        /// </summary>
         public void Clear()
         {
             _collection.Clear();
         }
 
+        /// <summary>
+        /// Create an array of elements
+        /// </summary>
+        /// <returns>returns an array</returns>
         public JpegFragment[] ToArray()
         {
             return _collection.ToArray();
         }
 
+        /// <summary>
+        /// Enqueue an element
+        /// </summary>
+        /// <param name="fragment">the fragment</param>
+        /// <exception cref="ArgumentNullException"/>
         public void Enqueue( JpegFragment fragment )
         {
             _collection.Enqueue( fragment ?? throw new ArgumentNullException( nameof( fragment ) ) );
         }
 
+        /// <summary>
+        /// Dequeue a fragment
+        /// </summary>
+        /// <returns>returns an instance, otherwise it throw an exception</returns>
+        /// <exception cref="InvalidOperationException"/>
         public JpegFragment Dequeue()
         {
             return _collection.Dequeue() ?? throw new InvalidOperationException();
         }
 
+        /// <summary>
+        /// Peek a fragment
+        /// </summary>
+        /// <returns>returns an instance, otherwise it thrown an exception</returns>
+        /// <exception cref="InvalidOperationException"/>
         public JpegFragment Peek()
         {
             return _collection.Peek() ?? throw new InvalidOperationException();
         }
 
+        /// <summary>
+        /// Try to enqueue an element
+        /// </summary>
+        /// <param name="fragment">the fragment</param>
+        /// <returns>returns true for a success, otherwise false</returns>
         public bool TryEnqueue( JpegFragment fragment )
         {
             if ( fragment == null )
@@ -115,6 +174,11 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
             return true;
         }
 
+        /// <summary>
+        /// Try to dequeue an element
+        /// </summary>
+        /// <param name="result">the result</param>
+        /// <returns>returns true for a success, otherwise false</returns>
         public bool TryDequeue( out JpegFragment result )
         {
             result = _collection.Count > 0 ? _collection.Dequeue() : null;
@@ -122,10 +186,15 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
             return result != null;
         }
 
+        /// <summary>
+        /// Try to peek an element
+        /// </summary>
+        /// <param name="result">the result</param>
+        /// <returns>returns true for a success, otherwise false</returns>
         public bool TryPeek( out JpegFragment result )
         {
-            result = _collection.Count > 0 ? _collection.Peek() : null ;
-            
+            result = _collection.Count > 0 ? _collection.Peek() : null;
+
             return result != null;
         }
     }
