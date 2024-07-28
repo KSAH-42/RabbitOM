f1118f785f3800dd135c50e688174e1fa705b849
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-29 00:07:55 +0200

    add comments

--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegDataConverter.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegDataConverter.cs
@@ -2,8 +2,16 @@
 
 namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 {
+    /// <summary>
+    /// Represent a data converter
+    /// </summary>
     public static class JpegDataConverter
     {
+        /// <summary>
+        /// Just adapt the quantization factor to an anther value
+        /// </summary>
+        /// <param name="value">value</param>
+        /// <returns>returns a values</returns>
         public static int ConvertToFactor( int value )
         {
             value = value < 1 ? 1 : ( value > 99 ? 99 : value );
