5d67a369055f202417d5ef9335f0125984176d1c
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-28 11:29:46 +0200

    remove the ToString method

--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegFragment.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegFragment.cs
@@ -72,15 +72,6 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
             return Type > 0 && Width >= 2 && Height >= 2 && Data.Count > 0;
         }
 
-        /// <summary>
-        /// Format to string
-        /// </summary>
-        /// <returns>returns true for a success otherwise</returns>
-        public override string ToString()
-        {
-            return $"{Offset} {Type} {QFactor} {Width} {Height} {Dri} {Mbz} {QTable.Count} {Data.Count}";
-        }
-
 
 
 
