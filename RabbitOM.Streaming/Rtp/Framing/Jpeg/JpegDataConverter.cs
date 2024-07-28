af1b938863c6e09a90715378a61478f18b6e11bd
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-28 21:32:23 +0200

    move some code of the quantizer class to a common data converter class

---
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegDataConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
+{
+    public static class JpegDataConverter
+    {
+        public static int ConvertToFactor( int value )
+        {
+            value = value < 1 ? 1 : ( value > 99 ? 99 : value );
+
+            return value < 50 ? ( 5000 / value ) : ( 200 - value * 2 );
+        }
+    }
+}
--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegQuantizer.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegQuantizer.cs
@@ -4,7 +4,7 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 {
     public sealed class JpegQuantizer
     {
-        private static readonly byte[] DefaultQuantizer =
+        private static readonly byte[] BaseTable =
         {
             16, 11, 12, 14, 12, 10, 16, 14,
             13, 14, 18, 17, 16, 19, 24, 40,
@@ -29,32 +29,32 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 
         private ArraySegment<byte> _table;
 
-        private int _quantizationFactor;
+        private int _factor;
 
 
 
 
-        public ArraySegment<byte> CreateTable( int quantizationFactor )
+        public ArraySegment<byte> CreateTable( int factor )
         {
-            if ( _table.Count > 0 && _quantizationFactor == quantizationFactor )
+            if ( _table.Count > 0 && _factor == factor )
             {
                 return _table;
             }
 
-            byte[] buffer = new byte[ DefaultQuantizer.Length ];
+            byte[] buffer = new byte[ BaseTable.Length ];
 
-            int factor = AdaptQuantizationFactor( quantizationFactor );
+            int quantizationFactor = JpegDataConverter.ConvertToFactor( factor );
 
             for ( int i = 0 ; i < buffer.Length ; ++i )
             {
-                int newVal = ( DefaultQuantizer[ i ] * factor + 50 ) / 100;
+                int newVal = ( BaseTable[ i ] * quantizationFactor + 50 ) / 100;
 
                 buffer[ i ] = (newVal < 1) ? (byte) 0x01 : (newVal > 0xFF ) ? (byte) 0xFF : (byte) newVal;
             }
 
             _table = new ArraySegment<byte>( buffer , 0 , buffer.Length );
 
-            _quantizationFactor = quantizationFactor;
+            _factor = factor;
 
             return _table;
         }
@@ -64,11 +64,6 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 
 
 
-        private static int AdaptQuantizationFactor( int value )
-        {
-            value = value < 1 ? 1 : (value > 99 ? 99 : value);
-
-            return value < 50 ? ( 5000 / value ) : ( 200 - value * 2 );
-        }
+        
     }
 }
