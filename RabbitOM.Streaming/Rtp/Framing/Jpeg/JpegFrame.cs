aa04b5518d02d086160e3e061abca626d94f6ec0
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-29 00:13:57 +0200

    add comments

--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegFrame.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegFrame.cs
@@ -2,11 +2,20 @@
 
 namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 {
+    /// <summary>
+    /// Represent the a jpeg frame
+    /// </summary>
     public class JpegFrame : RtpFrame
     {
         private readonly int _width;
         private readonly int _height;
 
+        /// <summary>
+        /// Initialize a new instance of the jpeg frame
+        /// </summary>
+        /// <param name="width">the width</param>
+        /// <param name="height">the height</param>
+        /// <param name="data">the data</param>
         public JpegFrame( int width , int height , byte[] data )
             : base ( data )
         {
@@ -14,17 +23,28 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
             _height = height;
         }
 
+        /// <summary>
+        /// Gets the width
+        /// </summary>
         public int Width
         {
             get => _width;
         }
 
+        /// <summary>
+        /// Gets the height
+        /// </summary>
         public int Height
         {
             get => _height;
         }
 
 
+        /// <summary>
+        /// Create an new object
+        /// </summary>
+        /// <param name="image">the image data structure info</param>
+        /// <returns>returns an instance</returns>
         public static JpegFrame NewFrame( JpegImage image )
         {
             return new JpegFrame( image.Width , image.Height , image.Data );
