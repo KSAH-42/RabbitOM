e8ae647ae29ce71ad05c64827a8f055d948dbb92
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-29 10:44:40 +0200

    add comments

--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegImage.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegImage.cs
@@ -2,17 +2,36 @@
 
 namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 {
+    /// <summary>
+    /// Represent a simple data structure
+    /// </summary>
     public struct JpegImage
     {
+        /// <summary>
+        /// Empty value
+        /// </summary>
         public static readonly JpegImage Empty = new JpegImage();
 
 
+
+        
         private readonly byte[] _data;
+        
         private readonly int _width;
+        
         private readonly int _height;
 
 
 
+
+
+        /// <summary>
+        /// Initialize an instance
+        /// </summary>
+        /// <param name="data">the data</param>
+        /// <param name="width">the width</param>
+        /// <param name="height">the height</param>
+        /// <exception cref="ArgumentNullException"/>
         public JpegImage( byte[] data , int width , int height )
         {
             if ( data == null )
@@ -28,16 +47,27 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 
 
 
+
+
+        /// <summary>
+        /// Gets the image in binary form
+        /// </summary>
         public byte[] Data
         {
             get => _data;
         }
 
+        /// <summary>
+        /// Gets the width
+        /// </summary>
         public int Width
         {
             get => _width;
         }
 
+        /// <summary>
+        /// Gets the height
+        /// </summary>
         public int Height
         {
             get => _height;
--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegImageBuilder.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegImageBuilder.cs
@@ -2,37 +2,63 @@
 
 namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
 {
+    /// <summary>
+    /// Represent an image builder class
+    /// </summary>
     public sealed class JpegImageBuilder : IDisposable
     {
         private readonly JpegStreamWriter _writer = new JpegStreamWriter();
         private readonly JpegFragmentQueue _fragments = new JpegFragmentQueue();
        
+        /// <summary>
+        /// Dispose
+        /// </summary>
         public void Dispose()
         {
             _writer.Dispose();
             _fragments.Clear();
         }
 
+        /// <summary>
+        /// Check if the fragment can be added
+        /// </summary>
+        /// <param name="fragment">the fragment</param>
+        /// <returns>returns true for a success, otherwise false</returns>
         public bool CanAddFragment( JpegFragment fragment )
         {
             return fragment != null && fragment.TryValidate();
         }
 
+        /// <summary>
+        /// Add a fragment
+        /// </summary>
+        /// <param name="fragment">the fragment</param>
         public void AddFragment( JpegFragment fragment )
         {
             _fragments.Enqueue( fragment );
         }
 
+        /// <summary>
+        /// Removes all fragments
+        /// </summary>
         public void Clear()
         {
             _fragments.Clear();
         }
 
+        /// <summary>
+        /// Check if an image can be build
+        /// </summary>
+        /// <returns>returns true for a success, otherwise false</returns>
         public bool CanBuildImage()
         {
             return _fragments.Any();
         }
 
+        /// <summary>
+        /// Build the image from the fragments
+        /// </summary>
+        /// <returns>returns an instance</returns>
         public JpegImage BuildImage()
         {
             var firstFragment = _fragments.Dequeue();
