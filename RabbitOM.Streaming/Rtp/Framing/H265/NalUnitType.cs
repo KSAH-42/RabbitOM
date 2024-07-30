78704e523709a4145460cfdc66af4c2956fcc92f
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-30 19:20:18 +0200

    Changing enums and add comments

--- RabbitOM.Streaming/Rtp/Framing/H265/NalUnitType.cs
+++ RabbitOM.Streaming/Rtp/Framing/H265/NalUnitType.cs
@@ -4,34 +4,169 @@ namespace RabbitOM.Streaming.Rtp.Framing.H265
 {
 	public enum NalUnitType : byte
 	{
-		INVALID = -1,
-		TRAIL_N = 0,
-		TRAIL_R = 1,
-		TSA_N = 2,
-		TSA_R = 3,
-		STSA_N = 4,
-		STSA_R = 5,
-		RADL_N = 6,
-		RADL_R = 7,
-		RASL_N = 8,
-		RASL_R = 9,
-		BLA_W_LP = 0X10,
-		BLA_W_RADL = 17,
-		BLA_N_LP = 18,
-		IDR_W_RADL = 19,
-		IDR_N_LP = 20,
-		CRA = 21,
-		VPS = 0X20,
-		SPS = 33,
-		PPS = 34,
-		AUD = 35,
-		EOS = 36,
-		EOB = 37,
-		FILLER = 38,
-		PREFIXSEI = 39,
-		SUFSEI = 40,
-		AGGREGATE = 48,
-		FRAGMENT = 49,
-		PACI = 50
+		/// <summary>
+		/// Coded slice segment of a non-TSA, non-STSA trailing picture (Non-IDR slice of a non-TSA, non-STSA picture).
+		/// </summary>
+		TRAIL_N = 0 ,
+
+		/// <summary>
+		/// Coded slice segment of a non-TSA, non-STSA trailing picture (IDR slice of a non-TSA, non-STSA picture).
+		/// </summary>
+		TRAIL_R = 1 ,
+
+		/// <summary>
+		/// Coded slice segment of a TSA (Temporal Sub-layer Access) picture.
+		/// </summary>
+		TSA_N = 2 ,
+
+		/// <summary>
+		/// Coded slice segment of a TSA (Temporal Sub-layer Access) picture with RADL (Referenceable Arrival Decoding).
+		/// </summary>
+		TSA_R = 3 ,
+
+		/// <summary>
+		/// Coded slice segment of an STSA (Step-wise Temporal Sub-layer Access) picture.
+		/// </summary>
+		STSA_N = 4 ,
+
+		/// <summary>
+		/// Coded slice segment of an STSA (Step-wise Temporal Sub-layer Access) picture with RADL.
+		/// </summary>
+		STSA_R = 5 ,
+
+		/// <summary>
+		/// Coded slice segment of a RADL (Referenceable Arrival Decoding) picture.
+		/// </summary>
+		RADL_N = 6 ,
+
+		/// <summary>
+		/// Coded slice segment of a RADL (Referenceable Arrival Decoding) picture with RADL.
+		/// </summary>
+		RADL_R = 7 ,
+
+		/// <summary>
+		/// Coded slice segment of a RASL (Referenceable Arrival Decoding) picture.
+		/// </summary>
+		RASL_N = 8 ,
+
+		/// <summary>
+		/// Coded slice segment of a RASL (Referenceable Arrival Decoding) picture with RADL.
+		/// </summary>
+		RASL_R = 9 ,
+
+		/// <summary>
+		/// Reserved non-IRAP (Instantaneous Refresh Access Point) NAL unit types.
+		/// </summary>
+		RSV_VCL_N10 = 10 ,
+
+		/// <summary>
+		/// Reserved IRAP NAL unit types.
+		/// </summary>
+		RSV_VCL_R11 = 11 ,
+
+		/// <summary>
+		/// Reserved non-IRAP NAL unit types.
+		/// </summary>
+		RSV_VCL_N12 = 12 ,
+
+		/// <summary>
+		/// Reserved IRAP NAL unit types.
+		/// </summary>
+		RSV_VCL_R13 = 13 ,
+
+		/// <summary>
+		/// Reserved non-IRAP NAL unit types.
+		/// </summary>
+		RSV_VCL_N14 = 14 ,
+
+		/// <summary>
+		/// Reserved IRAP NAL unit types.
+		/// </summary>
+		RSV_VCL_R15 = 15 ,
+
+		/// <summary>
+		/// Coded slice segment of a BLA (Broken Link Access) picture with leading picture.
+		/// </summary>
+		BLA_W_LP = 16 ,
+
+		/// <summary>
+		/// Coded slice segment of a BLA (Broken Link Access) picture with RADL.
+		/// </summary>
+		BLA_W_RADL = 17 ,
+
+		/// <summary>
+		/// Coded slice segment of a BLA (Broken Link Access) picture with no leading picture.
+		/// </summary>
+		BLA_N_LP = 18 ,
+
+		/// <summary>
+		/// Coded slice segment of an IDR (Instantaneous Decoding Refresh) picture with RADL.
+		/// </summary>
+		IDR_W_RADL = 19 ,
+
+		/// <summary>
+		/// Coded slice segment of an IDR (Instantaneous Decoding Refresh) picture with no leading picture.
+		/// </summary>
+		IDR_N_LP = 20 ,
+
+		/// <summary>
+		/// Coded slice segment of a CRA (Clean Random Access) picture.
+		/// </summary>
+		CRA_NUT = 21 ,
+
+		/// <summary>
+		/// Reserved IRAP NAL unit types.
+		/// </summary>
+		RSV_IRAP_VCL22 = 22 ,
+
+		/// <summary>
+		/// Reserved IRAP NAL unit types.
+		/// </summary>
+		RSV_IRAP_VCL23 = 23 ,
+
+		/// <summary>
+		/// Video parameter set.
+		/// </summary>
+		VPS_NUT = 24 ,
+
+		/// <summary>
+		/// Sequence parameter set.
+		/// </summary>
+		SPS_NUT = 25 ,
+
+		/// <summary>
+		/// Picture parameter set.
+		/// </summary>
+		PPS_NUT = 26 ,
+
+		/// <summary>
+		/// Access unit delimiter.
+		/// </summary>
+		AUD_NUT = 27 ,
+
+		/// <summary>
+		/// End of sequence.
+		/// </summary>
+		EOS_NUT = 28 ,
+
+		/// <summary>
+		/// End of bitstream.
+		/// </summary>
+		EOB_NUT = 29 ,
+
+		/// <summary>
+		/// Filler data.
+		/// </summary>
+		FD_NUT = 30 ,
+
+		/// <summary>
+		/// Supplemental enhancement information (SEI).
+		/// </summary>
+		PREFIX_SEI_NUT = 31 ,
+
+		/// <summary>
+		/// Supplemental enhancement information (SEI).
+		/// </summary>
+		SUFFIX_SEI_NUT = 32 ,
 	}
 }
