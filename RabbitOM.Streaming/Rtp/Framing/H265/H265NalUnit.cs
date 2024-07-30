c89abeda1e4a6dba6382dc1bc136360bb5d6c690
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-31 00:26:18 +0200

    just rearrange comments

--- RabbitOM.Streaming/Rtp/Framing/H265/H265NalUnit.cs
+++ RabbitOM.Streaming/Rtp/Framing/H265/H265NalUnit.cs
@@ -36,7 +36,7 @@ namespace RabbitOM.Streaming.Rtp.Framing.H265
 
 
         /*
-           Please note that from the rfc, bit order is not like 7 6 5 4 3 2 1 0 | 7 6 5 4 3 2 1 0 
+            Please note that from the rfc, bit order is not like 7 6 5 4 3 2 1 0 | 7 6 5 4 3 2 1 0 
 
             +---------------+---------------+
             |0|1|2|3|4|5|6|7|0|1|2|3|4|5|6|7|
