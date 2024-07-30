ae435c6faed6ea3fa37fe12ea4137e7ed0f2b5fd
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-07-30 11:04:22 +0200

    introduce base configuration class for frame builder

--- RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegFrameBuilderConfiguration.cs
+++ RabbitOM.Streaming/Rtp/Framing/Jpeg/JpegFrameBuilderConfiguration.cs
@@ -5,76 +5,8 @@ namespace RabbitOM.Streaming.Rtp.Framing.Jpeg
     /// <summary>
     /// Represent the builder configuration class
     /// </summary>
-    public sealed class JpegFrameBuilderConfiguration
+    public sealed class JpegFrameBuilderConfiguration : FrameBuilderConfiguration
     {
-        /// <summary>
-        /// The default maximum payload size
-        /// </summary>
-        public const int DefaultMaximumPayloadSize = 1500 * 3;
-
-        /// <summary>
-        /// The default number of packets per frame
-        /// </summary>
-        public const int DefaultNumberOfPacketsPerFrame = 1000;
-
-
-
-
-
-
-        private readonly object _lock = new object();
-
-        private int _maximumPayloadSize      = DefaultMaximumPayloadSize;
         
-        private int _numberOfPacketsPerFrame = DefaultNumberOfPacketsPerFrame;
-
-
-
-
-
-
-        /// <summary>
-        /// Gets / Sets the maximul payload size
-        /// </summary>
-        public int MaximumPayloadSize
-        {
-            get
-            {
-                lock ( _lock )
-                {
-                    return _maximumPayloadSize;
-                }
-            }
-
-            set
-            {
-                lock ( _lock )
-                {
-                    _maximumPayloadSize = value;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Gets / Sets the numbers of packets allowed per frame
-        /// </summary>
-        public int NumberOfPacketsPerFrame
-        {
-            get
-            {
-                lock ( _lock )
-                {
-                    return _numberOfPacketsPerFrame;
-                }
-            }
-
-            set
-            {
-                lock ( _lock )
-                {
-                    _numberOfPacketsPerFrame = value;
-                }
-            }
-        }
     }
 }
---
+++ RabbitOM.Streaming/Rtp/Framing/RtpFrameBuilderConfiguration.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Rtp.Framing
+{
+    /// <summary>
+    /// Represent the builder configuration class
+    /// </summary>
+    public class FrameBuilderConfiguration
+    {
+        /// <summary>
+        /// The default maximum payload size
+        /// </summary>
+        public const int DefaultMaximumPayloadSize = 1500 * 3;
+
+        /// <summary>
+        /// The default number of packets per frame
+        /// </summary>
+        public const int DefaultNumberOfPacketsPerFrame = 1000;
+
+
+
+
+
+
+        private readonly object _lock = new object();
+
+        private int _maximumPayloadSize      = DefaultMaximumPayloadSize;
+        
+        private int _numberOfPacketsPerFrame = DefaultNumberOfPacketsPerFrame;
+
+
+
+
+
+
+        /// <summary>
+        /// Gets the sync root
+        /// </summary>
+        protected object SyncRoot
+        {
+            get => _lock;
+        }
+
+        /// <summary>
+        /// Gets / Sets the maximul payload size
+        /// </summary>
+        public int MaximumPayloadSize
+        {
+            get => GetField( ref _maximumPayloadSize );
+            set => SetField( ref _maximumPayloadSize , value );
+        }
+
+        /// <summary>
+        /// Gets / Sets the numbers of packets allowed per frame
+        /// </summary>
+        public int NumberOfPacketsPerFrame
+        {
+            get => GetField( ref _numberOfPacketsPerFrame );
+            set => SetField( ref _numberOfPacketsPerFrame , value );
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// Get the field value using a lock
+        /// </summary>
+        /// <typeparam name="TValue">type of the value</typeparam>
+        /// <param name="memberValue">the member value</param>
+        /// <returns>returns a value</returns>
+        protected TValue GetField<TValue>( ref TValue memberValue )
+        {
+            lock ( _lock )
+            {
+                return memberValue;
+            }
+        }
+
+        /// <summary>
+        /// Set the field value using a lock
+        /// </summary>
+        /// <typeparam name="TValue">type of the value</typeparam>
+        /// <param name="memberValue">the member value</param>
+        /// <param name="value">the value</param>
+        protected void SetField<TValue>( ref TValue memberValue , TValue value )
+        {
+            lock ( _lock )
+            {
+                memberValue = value;
+            }
+        }
+    }
+}
