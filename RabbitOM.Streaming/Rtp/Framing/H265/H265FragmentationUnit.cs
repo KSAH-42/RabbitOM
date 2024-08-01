fae10732013d109b7130ccc61f24c0a8972d36df
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-08-01 19:52:56 +0200

    continue the implementation

---
+++ RabbitOM.Streaming/Rtp/Framing/H265/H265FragmentationUnit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Rtp.Framing.H265
+{
+    public struct H265FragmentationUnit
+    {
+        public bool StartBit { get; set; }
+        
+        public bool EndBit { get; set; }
+        
+        public byte Type { get; set; }
+
+        public ArraySegment<byte> Data { get; set; }
+
+
+
+
+        public bool TryValidate()
+        {
+            return Data.Count >= 1;
+        }
+
+
+
+
+        public static bool TryParse( ArraySegment<byte> buffer , out H265FragmentationUnit result )
+        {
+            result = default;
+
+            if ( buffer.Count <= 1 )
+            {
+                return false;
+            }
+
+            result = new H265FragmentationUnit();
+
+            result.StartBit = ( ( buffer.Array[ buffer.Offset ] >> 7 ) & 0x1 ) == 1;
+            result.EndBit   = ( ( buffer.Array[ buffer.Offset ] >> 6 ) & 0x1 ) == 1;
+
+            result.Type     = (byte) ( buffer.Array[ buffer.Offset ] & 0x3F );
+
+            result.Data     = new ArraySegment<byte>( buffer.Array , buffer.Offset + 1 , buffer.Count - 1 );
+
+            return true;
+        }
+    } 
+}
---
+++ RabbitOM.Streaming/Rtp/Framing/H265/H265StreamWriter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Rtp.Framing.H265
+{
+    public sealed class H265StreamWriter : IDisposable
+    {
+        private static readonly byte[] StartMarker = { 0x00 , 0x00 , 0x01 };
+
+        
+        private readonly RtpMemoryStream _stream = new RtpMemoryStream();
+
+
+
+
+
+
+        public long Length
+        {
+            get => _stream.Length;
+        }
+
+
+
+
+
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+
+        public void Write( ArraySegment<byte> data )
+        {
+            if ( data.Count == 0 )
+            {
+                throw new ArgumentException( nameof( data ) );
+            }
+
+            _stream.WriteAsBinary( data );
+        }
+
+        public void WriteStartMarker()
+        {
+            _stream.WriteAsBinary( StartMarker );
+        }
+
+        public void WriteParameterSet( ArraySegment<byte> data )
+        {
+            if ( data.Count == 0 )
+            {
+                throw new ArgumentException( nameof( data ) );
+            }
+
+            _stream.WriteAsBinary( StartMarker );
+            _stream.WriteAsBinary( data.Array , data.Offset , data.Count );
+        }
+
+        public byte[] ToArray()
+        {
+            return _stream.ToArray();
+        }
+    }
+}
