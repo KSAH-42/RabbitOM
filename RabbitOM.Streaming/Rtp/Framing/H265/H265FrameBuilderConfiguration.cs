76f71a337ffc835f7e0a3d251b926fa91a4aeead
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-08-03 13:52:29 +0200

    add comments

--- RabbitOM.Streaming/Rtp/Framing/H265/H265FrameBuilderConfiguration.cs
+++ RabbitOM.Streaming/Rtp/Framing/H265/H265FrameBuilderConfiguration.cs
@@ -2,6 +2,7 @@
 
 namespace RabbitOM.Streaming.Rtp.Framing.H265
 {
+    // TODO: remove this  class
     public sealed class H265FrameBuilderConfiguration : RtpFrameBuilderConfiguration
     {
     }
