fe1117c12f8289eb137f84e9f1f6ed59bb1b5c47
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-08-14 09:54:09 +0200

    remove some code

--- RabbitOM.Streaming/Rtp/Framing/H265/H265FrameFactory.cs
+++ RabbitOM.Streaming/Rtp/Framing/H265/H265FrameFactory.cs
@@ -9,12 +9,10 @@ namespace RabbitOM.Streaming.Rtp.Framing.H265
 
         public void Dispose()
         {
-            _reassembler.Dispose();
         }
 
         public void Clear()
         {
-            _reassembler.Clear();
         }
 
         public bool TryCreateFrames( IEnumerable<RtpPacket> packets , out IEnumerable<RtpFrame> result )
@@ -24,16 +22,6 @@ namespace RabbitOM.Streaming.Rtp.Framing.H265
             if ( packets == null )
             {
                 return false;
-            }
-
-            _reassembler.Clear();
-
-            foreach ( RtpPacket packet in packets )
-            {
-                if ( H265NalUnit.TryParse( packet.Payload , out H265NalUnit nalUnit ) )
-                {
-                    _reassembler.AddNalUnit( nalUnit );
-                }
             }
 
             throw new NotImplementedException();
