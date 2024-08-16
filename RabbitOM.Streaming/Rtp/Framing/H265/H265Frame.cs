18216817991642d91dfd8739ce9a9311c7795549
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-08-17 00:51:37 +0200

    remove exception throwing

--- RabbitOM.Streaming/Rtp/Framing/H265/H265Frame.cs
+++ RabbitOM.Streaming/Rtp/Framing/H265/H265Frame.cs
@@ -4,31 +4,16 @@ namespace RabbitOM.Streaming.Rtp.Framing.H265
 {
     public sealed class H265Frame : RtpFrame
     {
-        private readonly byte[] _vps;
-        private readonly byte[] _sps;
-        private readonly byte[] _pps;
-
         public H265Frame( byte[] data , byte[] vps , byte[] sps , byte[] pps ) 
             : base ( data )
         {
-            _vps = vps ?? throw new ArgumentNullException( nameof( vps ) );
-            _sps = sps ?? throw new ArgumentNullException( nameof( sps ) );
-            _pps = pps ?? throw new ArgumentNullException( nameof( pps ) );
+            PPS = pps;
+            SPS = sps;
+            VPS = vps;
         }
 
-        public byte[] VPS
-        {
-            get => _vps;
-        }
-
-        public byte[] SPS
-        {
-            get => _sps;
-        }
-
-        public byte[] PPS
-        {
-            get => _pps;
-        }
+        public byte[] PPS { get; }
+        public byte[] SPS { get; }
+        public byte[] VPS { get; }
     }
 }
