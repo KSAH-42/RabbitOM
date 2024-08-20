2963ea0bc77ec87f4535debb06e88e12a818bb55
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-08-20 16:06:52 +0200

    move extensions methods to a different static class

--- RabbitOM.Streaming/ArrayExtensions.cs
+++ RabbitOM.Streaming/ArrayExtensions.cs
@@ -7,10 +7,6 @@ namespace RabbitOM.Streaming
     /// </summary>
     public static class ArrayExtensions
     {
-        private static readonly byte[] s_empty_buffer = new byte[0];
-
-
-
         /// <summary>
         /// Byte array comparison
         /// </summary>
@@ -38,78 +34,6 @@ namespace RabbitOM.Streaming
             }
 
             return true;
-        }
-
-        /// <summary>
-        /// Array segment comparison
-        /// </summary>
-        /// <param name="source">the source</param>
-        /// <param name="target">the target</param>
-        /// <returns>returns true for a success, otherwise false</returns>
-        public static bool IsEqualTo( this ArraySegment<byte> source , ArraySegment<byte> target )
-        {
-            if ( object.ReferenceEquals( source.Array , target.Array ) )
-            {
-                return true;
-            }
-
-            if ( source.Array == null || target.Array == null || source.Count != target.Count )
-            {
-                return false;
-            }
-
-            for ( int i = 0 ; i < source.Count ; ++i )
-            {
-                if ( source.Array[ i + source.Offset ] != target.Array[ i + target.Offset ] )
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Check if the byte array
-        /// </summary>
-        /// <param name="source">the source</param>
-        /// <param name="target">the target</param>
-        /// <returns>returns true for a success, otherwise false</returns>
-        public static bool StartsWith( this ArraySegment<byte> source , byte[] target )
-        {
-            if ( source.Array == null || source.Count < target.Length )
-            {
-                return false;
-            }
-
-            for ( int i = 0 ; i < target.Length ; ++ i )
-            {
-                if ( target[ i ] != source.Array[ i + source.Offset ] )
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Create a bytes array from an array segment
-        /// </summary>
-        /// <param name="source">the source</param>
-        /// <returns>returns an array</returns>
-        public static byte[] ToArray( this ArraySegment<byte> source )
-        {
-            if ( source.Count == 0 )
-            {
-                return s_empty_buffer;
-            }
-
-            byte[] result = new byte[ source.Count ];
-
-            Buffer.BlockCopy( source.Array , source.Offset , result , 0 , result.Length );
-
-            return result;
         }
     }
 }
