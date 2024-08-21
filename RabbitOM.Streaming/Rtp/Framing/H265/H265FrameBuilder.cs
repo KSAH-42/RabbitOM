4cefa81825466a5617f9cfd60fe79b0564f56080
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-08-22 00:23:57 +0200

    change comments

--- RabbitOM.Streaming/Rtp/Framing/H265/H265FrameBuilder.cs
+++ RabbitOM.Streaming/Rtp/Framing/H265/H265FrameBuilder.cs
@@ -20,7 +20,7 @@ namespace RabbitOM.Streaming.Rtp.Framing.H265
         public H265FrameBuilder()
         {
 #if !DEBUG
-            throw new NotImplementedException( "the implementation is not finished, this class must not be used in production." );
+            throw new NotImplementedException( "the implementation is not yet finished, this class must not be used in production." );
 #endif
             _configuration = new H265FrameBuilderConfiguration();
             _frameFactory  = new H265FrameFactory( this );
