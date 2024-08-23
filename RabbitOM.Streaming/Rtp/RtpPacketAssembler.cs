6eedebddc7e42f141a6d1d0b26bb8b056322f32a
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-08-23 23:37:50 +0200

    add a rtpe packet assembler class

---
+++ RabbitOM.Streaming/Rtp/RtpPacketAssembler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RabbitOM.Streaming.Rtp
+{
+    public sealed class RtpPacketAssembler
+    {
+        private readonly RtpPacketQueue _packets = new RtpPacketQueue();
+ 
+
+
+
+
+        public IReadOnlyCollection<RtpPacket> Packets
+        {
+            get => _packets;
+        }
+
+
+
+
+
+        public bool TryAssemble( RtpPacket packet , out IEnumerable<RtpPacket> result )
+        {
+            result = null;
+
+            if ( packet == null )
+            {
+                return false;
+            }
+
+            _packets.Enqueue( packet );
+
+            if ( packet.Marker )
+            {
+                result = RtpPacketQueue.CanSort( _packets ) ? RtpPacketQueue.Sort( _packets ) : _packets.AsEnumerable();
+
+                _packets.Clear();
+            }
+
+            return result != null;
+        }
+
+        public void Clear()
+        {
+            _packets.Clear();
+        }
+    }
+}
