44d61b02f986d86056420e570538f0b6fc242150
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-08-25 01:52:09 +0200

    rename variable and move comments outside a method

--- RabbitOM.Streaming/Rtp/Framing/H265/H265FrameFactory.cs
+++ RabbitOM.Streaming/Rtp/Framing/H265/H265FrameFactory.cs
@@ -76,24 +76,20 @@ namespace RabbitOM.Streaming.Rtp.Framing.H265
 
 
 
+        // TODO: a code refactoring must done here 
+        // This code here is not yet finished !!!
+        // Because we need to parse first  2 byte of
+        // the rtp payload to see if it is a nal unit header
+        // or fragment header
+        // before parsing 
+        // So here we can continue to used to parse the nalunit
+        // but we to test the forbiddenBit true
+        // in order to used a different parser  
+        // to parse and to test the unitnalUnit.ForbiddenBit == true
+        // this code is actually incompleted
+
         private void HandlePacket( RtpPacket packet )
         {
-            // TODO: a code refactoring must done here 
-
-            // This code here is not yet finished !!!
-
-
-            // Because we need to parse first  2 byte of
-            // the rtp payload to see if it is a nal unit header
-            // or fragment header
-            // before parsing 
-            // So here we can continue to used to parse the nalunit
-            // but we to test the forbiddenBit true
-            // in order to used a different parser  
-            // to parse and to test the unitnalUnit.ForbiddenBit == true
-            // this code is actually incompleted
-
-
             if ( ! _converter.TryConvert( packet , out H265NalUnit nalUnit ) )
             {
                 OnError( packet );
@@ -167,9 +163,9 @@ namespace RabbitOM.Streaming.Rtp.Framing.H265
 
         private void OnHandleAggregation( RtpPacket packet , H265NalUnit nalUnit )
         {
-            foreach ( var smallNalUnit in nalUnit.GetAggregationUnits() )
+            foreach ( var nalu in nalUnit.GetAggregationUnits() )
             {
-                _streamBuilder.Write( smallNalUnit );
+                _streamBuilder.Write( nalu );
             }
         }
 
