e58e45680d402f0b6762de6838be94e7a2a46844
Kader SAHNINE <a.sahnine@netcourrier.com>
2024-09-11 23:24:33 +0200

    turn the scope class as a struct

--- RabbitOM.Streaming/Rtsp/RtspQueue.Scope.cs
+++ RabbitOM.Streaming/Rtsp/RtspQueue.Scope.cs
@@ -10,7 +10,7 @@ namespace RabbitOM.Streaming.Rtsp
         /// <summary>
         /// This class is used for updating the internal event handle during add and remove operations
         /// </summary>
-        sealed class Scope : IDisposable
+        struct Scope : IDisposable
         {
             private readonly RtspQueue<TElement> _queue;
 
