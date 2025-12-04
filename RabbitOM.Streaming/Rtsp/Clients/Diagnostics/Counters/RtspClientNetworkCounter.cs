0f6fe7a42805e7307d0dc7b94a041e3eca64c86f
Kader SAHNINE <a.sahnine@netcourrier.com>
2025-12-04 23:07:37 +0100

    add performance counter not yet finished

---
+++ RabbitOM.Streaming/Rtsp/Clients/Diagnostics/Counters/RtspClientNetworkCounter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Rtsp.Clients.Diagnostics.Counters
+{
+    public sealed class RtspClientNetworkCounter : RtspClientCounter
+    {
+        private RtspClientNetworkCounter( string name ) : base ( name )
+        {
+        }
+
+        private RtspClientNetworkCounter( string name , string description ) : base ( name , description )
+        {
+        }
+        
+
+
+
+
+        public DateTime LastTimeCommunicationStart { get; }
+        public DateTime LastTimeCommunicationStop { get; }
+        public DateTime LastTimeConnection { get; }
+        public DateTime LastTimeConnectionError { get; }
+        public DateTime LastTimeDisconnection { get; }
+        public DateTime LastTimeDisconnectionError { get; }
+        public DateTime LastTimeDataReceived { get; }
+        public string CodecType { get; }
+        public string MediaType { get; }
+        public string MediaTransportType { get; }
+        public int NumberOfConnections { get; }
+        public int NumberOfConnectionsError { get; }
+        public int NumberOfPacketsReceived { get; }
+        public int NumberOfPacketsLost { get; }
+        public int NumberOfErrors { get; }
+        public int FrameRatePerSeconds { get; }
+        public int BitRatePerSeconds { get; }
+        public int InputBound { get; }
+        
+
+
+
+
+        public void IncreaseCommunicationStart()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void IncreaseCommunicationStop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncreaseConnectionSucceed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncreaseConnectionError()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncreaseDisconnectionSucceed()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void IncreaseDisconnectionError()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void SetBuffer( byte[] buffer )
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void SetMediaInfo(string codec, string mediaType, string mediaTransport)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
