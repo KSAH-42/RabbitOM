061d060e43c12ec9e16a4625a71c5016e20462dd
Kader SAHNINE <a.sahnine@netcourrier.com>
2025-12-17 17:29:27 +0100

    add H264 nalu types

---
+++ RabbitOM.Streaming/Net/Rtp/H264/Headers/NalUnitType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Net.Rtp.H264.Headers
+{
+    public enum NalUnitType
+	{
+        NALU_TYPE_UNKNOWN = 0,
+        NALU_TYPE_SLICE = 1,
+        NALU_TYPE_DPA = 2,
+        NALU_TYPE_DPB = 3,
+        NALU_TYPE_DPC = 4,
+        NALU_TYPE_IDR = 5,
+        NALU_TYPE_SEI = 6,
+        NALU_TYPE_SPS = 7,
+        NALU_TYPE_PPS = 8,
+        NALU_TYPE_AUD = 9,
+        NALU_TYPE_EOSEQ = 10,
+        NALU_TYPE_EOSTREAM = 11,
+        NALU_TYPE_FILL = 12,
+    }
+}
