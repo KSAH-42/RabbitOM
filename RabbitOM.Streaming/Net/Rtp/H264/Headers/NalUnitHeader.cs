24d8dc7e9d3454a29ec8c6f7586a5b6cf27a185e
Kader SAHNINE <a.sahnine@netcourrier.com>
2025-12-17 18:10:48 +0100

    adding H264 nalu header

---
+++ RabbitOM.Streaming/Net/Rtp/H264/Headers/NalUnitHeader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Net.Rtp.H264.Headers
+{
+    public struct NalUnitHeader
+    {
+        public bool ForbiddenBit { get; private set; }
+        public NalUnitType Type { get; private set; }
+        public byte Nri { get; private set; }
+        public ArraySegment<byte> Payload { get; private set; }
+        
+
+
+        
+
+
+        
+
+        public static bool IsInvalidOrUnDefined( ref NalUnitHeader header )
+        {
+            return header.Type == NalUnitType.NALU_TYPE_UNKNOWN;
+        }
+
+        // https://datatracker.ietf.org/doc/html/rfc6184#section-1.3
+        
+        //      header            payload can be null
+        //  +---------------+  +-----------------------+
+        //  |7|6|5|4|3|2|1|0|  |                       |
+        //  +-+-+-+-+-+-+-+-+  |                       |
+        //  |F|NRI|  Type   |  |                       |
+        //  +---------------+  +-----------------------+
+            
+        public static bool TryParse( ArraySegment<byte> buffer , out NalUnitHeader result )
+        {
+            result = default;
+
+            if ( buffer.Count < 1 )
+            {
+                return false;
+            }
+
+            var header = buffer.Array[ buffer.Offset ];
+            
+            result = new NalUnitHeader();
+
+            result.ForbiddenBit  = (byte)        ( ( header >> 7 ) & 0x01 ) == 1;
+            result.Nri           = (byte)        ( ( header >> 5 ) & 0x03 );
+            result.Type          = (NalUnitType) (   header        & 0x1F );
+
+            if ( buffer.Count > 1 )
+            {
+                result.Payload = new ArraySegment<byte>( buffer.Array , buffer.Offset + 1 , buffer.Array.Length - ( buffer.Offset + 1 ) );
+            }
+
+            return true;
+        }
+    } 
+}
