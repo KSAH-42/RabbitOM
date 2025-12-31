eb43ddc4768410f59f800e2f0f1e12beaa22e1f5
Kader SAHNINE <a.sahnine@netcourrier.com>
2025-12-31 14:21:56 +0100

    adding rtcp packet

---
+++ RabbitOM.Streaming/Net/Rtcp/RtcpPacket.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Net.Rtcp
+{
+    public struct RtcpPacket
+    {
+        public int Version { get; private set; }
+        public bool Padding { get; private set; }
+        public byte ReceptionCount { get; private set; }
+        public RtcpPacketType Type { get; private set; }
+        public ushort Length { get;private set; }
+        public ArraySegment<byte> Payload { get; private set; }
+
+
+
+        public static bool IsWellFormed( ref RtcpPacket packet )
+        {
+            return packet.Version > 0 && packet.Type > 0 && packet.Payload.Count > 0;
+        }
+
+
+
+
+        /*
+          7 6  5  4 3 2 1 0 
+         +----------------+
+         | V | P |   RC   |
+         +----------------+
+         */
+        public static bool TryParse( ArraySegment<byte> buffer , out RtcpPacket result )
+        {
+            result = default;
+
+            if ( buffer.Count < 4 )
+            {
+                return false;
+            }
+
+            result = new RtcpPacket();
+            
+            result.Version        =  (buffer.Array[ buffer.Offset ] >> 6) & 0x3;
+
+            result.Padding        = ((buffer.Array[ buffer.Offset ] >> 5) & 0x1) == 1;
+
+            result.ReceptionCount =  (byte) (buffer.Array[ buffer.Offset ] & 0x1F);
+
+            result.Type           = (RtcpPacketType) buffer.Array[ buffer.Offset + 1 ];
+
+            result.Length         = (ushort) ( buffer.Array[ buffer.Offset + 2 ] * 0x100 + buffer.Array[ buffer.Offset + 3 ]);
+
+            if ( buffer.Count >= 5 )
+            {
+                result.Payload = new ArraySegment<byte>( buffer.Array , buffer.Offset + 4 , buffer.Array.Length - ( buffer.Offset + 4 ) );
+            }
+
+            return true;
+        }
+    }
+}
---
+++ RabbitOM.Streaming/Net/Rtcp/RtcpPacketType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Net.Rtcp
+{
+    public enum RtcpPacketType : byte
+    {
+        UNDEFINED = 0,
+        SR = 200 ,
+        RR = 201 ,
+        SDES = 202 ,
+        BYTE = 203 ,
+        APP = 204 ,
+    }
+}
