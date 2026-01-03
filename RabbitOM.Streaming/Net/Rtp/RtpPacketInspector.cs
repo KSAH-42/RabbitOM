e7552c866e709efe9e186442f540fe82cd9dcc71
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-01-03 23:15:55 +0100

    adding comments

--- RabbitOM.Streaming/Net/Rtp/RtpPacketInspector.cs
+++ RabbitOM.Streaming/Net/Rtp/RtpPacketInspector.cs
@@ -2,6 +2,9 @@
 
 namespace RabbitOM.Streaming.Net.Rtp
 {
+    /// <summary>
+    /// Represent a packet inspector. This component is used to detected errors, or something that potentially introduce issues.
+    /// </summary>
     public abstract class RtpPacketInspector
     {
         public abstract void Inspect( RtpPacket packet );
