b5aab10b690aae97c33a3f9f1639d3f48a351c5c
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-01-04 21:28:44 +0100

    adding a new rtp builder

---
+++ RabbitOM.Streaming/Net/Rtp/Pcm/L24SampleBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Net.Rtp.Pcm
+{
+    public class L24SampleBuilder : RtpSampleBuilder
+    {
+        protected override void OnPacketAdding( RtpPacketAddingEventArgs e )
+        {
+            var type = (int) e.Packet.Type;
+
+            e.CanContinue = 96 <= type && type <= 127;
+
+            base.OnPacketAdding( e );
+        }
+    }
+}
