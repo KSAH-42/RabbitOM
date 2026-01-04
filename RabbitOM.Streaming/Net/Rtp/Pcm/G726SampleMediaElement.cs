f81f41999aad881cc2f706ecd1085b7f0de1f8a3
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-01-04 02:12:19 +0100

    remove a metho

--- RabbitOM.Streaming/Net/Rtp/Pcm/G726SampleMediaElement.cs
+++ RabbitOM.Streaming/Net/Rtp/Pcm/G726SampleMediaElement.cs
@@ -8,24 +8,5 @@ namespace RabbitOM.Streaming.Net.Rtp.Pcm
             => BitRate = bitrate;
 
         public G726BitRate BitRate { get; }
-
-        public static int CountBits( G726SampleMediaElement sample )
-        {
-            if ( sample == null )
-            {
-                throw new ArgumentNullException( nameof( sample ) );
-            }
-
-            switch ( sample.BitRate )
-            {
-                case G726BitRate.BitRate_16000: return 2;
-                case G726BitRate.BitRate_24000: return 3;
-                case G726BitRate.BitRate_32000: return 4;
-                case G726BitRate.BitRate_40000: return 5;
-
-                default:
-                    throw new NotSupportedException();
-            }
-        }
     }
 }
