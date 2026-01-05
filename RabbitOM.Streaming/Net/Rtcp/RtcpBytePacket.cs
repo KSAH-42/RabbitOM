c682d4442f70c155cc2510ed0f0e426a98062c5d
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-01-05 13:08:56 +0100

    continue the implementation of rtcp

---
+++ RabbitOM.Streaming/Net/Rtcp/RtcpBytePacket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Net.Rtcp
+{
+    public struct RtcpBytePacket
+    {
+        public uint[] SynchronizationSourcesIds { get; private set; }
+
+
+
+
+
+        public static RtcpBytePacket Parse( in ArraySegment<byte> payload , int count )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RabbitOM.Streaming/Net/Rtcp/RtcpSdesPacket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Net.Rtcp
+{
+    public struct RtcpSdesPacket
+    {
+        public uint SynchronizationSourceId { get; private set; }
+
+        public long TimeStamp { get; private set; }
+
+
+
+
+        public static RtcpSdesPacket Parse( in ArraySegment<byte> payload )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
