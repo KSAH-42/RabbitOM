bcfb6f3abe628bce608763bd6f09baa45dfcff31
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-01-05 12:15:15 +0100

    changing exception and adding comments

--- RabbitOM.Streaming/MathHelper.cs
+++ RabbitOM.Streaming/MathHelper.cs
@@ -14,11 +14,12 @@ namespace RabbitOM.Streaming
         /// <param name="min">the min</param>
         /// <param name="max">the max</param>
         /// <returns>returns a value</returns>
+        /// <exception cref="ArgumentOutOfRangeException"/>
         public static long Clamp( in long value , in long min , in long max )
         {
             if ( min > max )
             {
-                throw new ArgumentException( nameof( min ) );
+                throw new ArgumentOutOfRangeException( nameof( min ) );
             }
 
             return value <= min ? min : value >= max ? max : value;
