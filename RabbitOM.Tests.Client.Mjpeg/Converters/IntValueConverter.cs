856a795eb4a33c32cf1df95597bd5b6edfe547ba
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-01-12 20:09:06 +0100

    adding converters

---
+++ RabbitOM.Tests.Client.Mjpeg/Converters/IntValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RabbitOM.Tests.Client.Mjpeg.Converters
+{
+    [ValueConversion(typeof(string),typeof(int))]
+    public sealed class IntValueConverter : IValueConverter
+    {
+        public object Convert( object value , Type targetType , object parameter , CultureInfo culture )
+        {
+             return value?.ToString() ?? "0";
+        }
+
+        public object ConvertBack( object value , Type targetType , object parameter , CultureInfo culture )
+        {
+           return int.TryParse( value as string , out var result ) ? result : 0;
+        }
+    }
+}
