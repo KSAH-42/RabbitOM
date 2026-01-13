5431ca9b75b242debb5a6d3ac108fac37d2e8368
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-01-13 17:03:59 +0100

    removing files

--- RabbitOM.Streaming/Net/Rtp/StartCodePrefixes.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace RabbitOM.Streaming.Net.Rtp
-{
-    internal static class StartCodePrefixes
-    {
-        public static readonly byte[] Default = new byte[] { 0x00 , 0x00 , 0x00 , 0x01 };
-    }
-}
