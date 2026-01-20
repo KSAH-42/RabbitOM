e0dc1e734dc721a359aacc9b8d5a0902e96e70f9
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-01-20 23:22:51 +0100

    changing comments

--- RabbitOM.Streaming/Net/Rtp/H266/H266FrameBuilder.cs
+++ RabbitOM.Streaming/Net/Rtp/H266/H266FrameBuilder.cs
@@ -6,7 +6,7 @@ namespace RabbitOM.Streaming.Net.Rtp.H266
     {
         private readonly H266FrameFactory _frameFactory = new H266FrameFactory();
 
-        public H266FrameBuilder() => throw new NotSupportedException("the implementation is not tested with multiple products and server, and it can not be used until this code has been removed");
+        public H266FrameBuilder() => throw new NotSupportedException("the implementation is not tested with multiple products and server, and it can not be used until this code will be tested with compliant server");
 
         public void Configure( H266FrameBuilderConfiguration configuration )
         {
