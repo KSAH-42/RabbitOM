d138429da5fdd5d142b355596e53f129bd82c314
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-02-10 19:28:45 +0100

    continue the implementation

--- RabbitOM.Streaming.Experimentation/Rtsp/Headers/AcceptRangesRtspHeader.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/AcceptRangesRtspHeader.cs
@@ -4,49 +4,44 @@ using System.Linq;
 
 namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers
 {
+    /// <summary>
+    /// Represent an rtsp header
+    /// </summary>
     public class AcceptRangesRtspHeader : RtspHeader 
     {
+        /// <summary>
+        /// The type name
+        /// </summary>
         public const string TypeName = "Accept-Ranges";
         
-        private readonly HashSet<string> _units = new HashSet<string>();
 
+
+
+
+        private readonly HashSet<string> _units = new HashSet<string>();
+        
+
+
+
+
+        /// <summary>
+        /// Gets the units
+        /// </summary>
         public IReadOnlyCollection<string> Units
         {
             get => _units;
         }
-
-        public bool TryAddUnit( string value )
-        {
-            var text = StringRtspNormalizer.Normalize( value );
-
-            if ( text.Any( x => char.IsLetterOrDigit( x ) ) || text == "*" )
-            {
-                return _units.Add( text );
-            }
-
-            return false;
-        }
-
-        public void RemoveUnit( string value )
-        {
-            _units.Remove( StringRtspNormalizer.Normalize( value ) );
-        }
-
-        public void RemoveUnits()
-        {
-            _units.Clear();
-        }
-
-        public override bool TryValidate()
-        {
-            return _units.Count > 0 && _units.All( StringRtspValidator.TryValidate );
-        }
-
-        public override string ToString()
-        {
-            return string.Join( ", " , _units );
-        }
         
+
+
+
+
+        /// <summary>
+        /// Try to parse
+        /// </summary>
+        /// <param name="input">the input</param>
+        /// <param name="result">the result</param>
+        /// <returns>returns true for a success, otherwise false</returns>
         public static bool TryParse( string input , out AcceptRangesRtspHeader result )
         {
             result = null;
@@ -71,6 +66,75 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers
             result = header;
 
             return true;
+        }
+
+
+
+
+
+        /// <summary>
+        /// Try to validate
+        /// </summary>
+        /// <returns>returns true for a success, otherwise false</returns>
+        public override bool TryValidate()
+        {
+            return _units.Count > 0 && _units.All( StringRtspValidator.TryValidate );
+        }
+
+        /// <summary>
+        /// Try to add an element
+        /// </summary>
+        /// <param name="value">the value</param>
+        /// <returns>returns true for a success, otherwise false</returns>
+        public bool TryAddUnit( string value )
+        {
+            var text = StringRtspNormalizer.Normalize( value );
+
+            if ( text.Any( x => char.IsLetterOrDigit( x ) ) || text == "*" )
+            {
+                return _units.Add( text );
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Add an element
+        /// </summary>
+        /// <param name="value">the value</param>
+        /// <exception cref="ArgumentException"/>
+        public void AddUnit( string value )
+        {
+            if ( ! TryAddUnit( value ) )
+            {
+                throw new ArgumentException( nameof( value ) );
+            }
+        }
+
+        /// <summary>
+        /// Remove an element
+        /// </summary>
+        /// <param name="value">the value</param>
+        public void RemoveUnit( string value )
+        {
+            _units.Remove( StringRtspNormalizer.Normalize( value ) );
+        }
+
+        /// <summary>
+        /// Remove all elements
+        /// </summary>
+        public void RemoveUnits()
+        {
+            _units.Clear();
+        }
+
+        /// <summary>
+        /// Format to string
+        /// </summary>
+        /// <returns>returns a string</returns>
+        public override string ToString()
+        {
+            return string.Join( ", " , _units );
         }
     }
 }
