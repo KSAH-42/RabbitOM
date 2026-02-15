1216dc7e3170aa308aa0fa95bc052c1fa853048c
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-02-15 01:48:02 +0100

    renaming

--- RabbitOM.Tests/Rtsp/Headers/Validation/HeaderValidatorTest.cs
+++ RabbitOM.Tests/Rtsp/Headers/Validation/HeaderValidatorTest.cs
@@ -101,5 +101,96 @@ namespace RabbitOM.Streaming.Tests.Rtsp.Headers.Validation
         {
             Assert.IsTrue( RtspHeaderValidator.TryValidateUri( text ) );
         }
+
+        [TestCase( null )]
+        [TestCase( "\0" )]
+        [TestCase( "\r" )]
+        [TestCase( "\n" )]
+        [TestCase( "\f" )]
+        [TestCase( "\v" )]
+        [TestCase( "\t" )]
+        [TestCase( "a\r" )]
+        [TestCase( "a\n" )]
+        [TestCase( "a\v" )]
+        [TestCase( "a\t" )]
+        [TestCase( "é" )]
+        [TestCase( "è")]
+        [TestCase( "à")]
+        [TestCase( "ù" )]
+        [TestCase( "ç" )]
+        [TestCase( "€" )]
+        [TestCase( "£" )]
+        [TestCase( "§" )]
+        [TestCase( "" )]
+        [TestCase( " " )]
+        [TestCase( "\rabcdef" )]
+        [TestCase( "abc\ndef123" )]
+        [TestCase( "AZ\nERTYUIOPQSDFGHJKLMWXCVBN0123456789" )]
+        [TestCase( " AZERTY\tUIOPQSDFGH JKLMWXCVBN0123456789 " )]
+        [TestCase( " AZERTY\vUIOPQSDFGH JKLMWXCVBN0123456789 " )]
+        [TestCase( " AZERTY\vUIOPQSDFGH JKLMWXCVBN0123456789\r\n" )]
+        public void TryValidateStringTestFailed( string text )
+        {
+            Assert.IsFalse( RtspHeaderValidator.TryValidateString( text ) );
+        }
+
+        
+        [TestCase( null )]
+        [TestCase( "\0" )]
+        [TestCase( "\r" )]
+        [TestCase( "\n" )]
+        [TestCase( "\f" )]
+        [TestCase( "\v" )]
+        [TestCase( "\t" )]
+        [TestCase( "a\r" )]
+        [TestCase( "a\n" )]
+        [TestCase( "a\v" )]
+        [TestCase( "a\t" )]
+        [TestCase( "é" )]
+        [TestCase( "è")]
+        [TestCase( "à")]
+        [TestCase( "ù" )]
+        [TestCase( "ç" )]
+        [TestCase( "€" )]
+        [TestCase( "£" )]
+        [TestCase( "§" )]
+        [TestCase( "" )]
+        [TestCase( " " )]
+        [TestCase( "\rabcdef" )]
+        [TestCase( "abc\ndef123" )]
+        [TestCase( "AZ\nERTYUIOPQSDFGHJKLMWXCVBN0123456789" )]
+        [TestCase( " AZERTY\tUIOPQSDFGH JKLMWXCVBN0123456789 " )]
+        [TestCase( " AZERTY\vUIOPQSDFGH JKLMWXCVBN0123456789 " )]
+        [TestCase( " AZERTY\vUIOPQSDFGH JKLMWXCVBN0123456789\r\n" )]
+        [TestCase("(")]
+        [TestCase(")")]
+        [TestCase("[")]
+        [TestCase("]")]
+        [TestCase("{")]
+        [TestCase("}")]
+        [TestCase("<")]
+        [TestCase(">")]
+        [TestCase(",")]
+        [TestCase(";")]
+        [TestCase(":")]
+        [TestCase("=")]
+        [TestCase("?")]
+        [TestCase(" ")]
+        [TestCase(" abcd ")]
+        [TestCase("ab cd")]
+        public void TryValidateTokenTestFailed( string text )
+        {
+            Assert.IsFalse( RtspHeaderValidator.TryValidateToken( text ) );
+        }
+
+        [TestCase( null )]
+        [TestCase("")]
+        [TestCase("  ")]
+        [TestCase("\r\nrtsp://admin:@127.0.0.1:554/screenlive")]
+        [TestCase("rtsp?/127.0.0.1:554/screenlive?a=1&b=2")]
+        public void TryValidateUriTestFailed( string text )
+        {
+            Assert.IsFalse( RtspHeaderValidator.TryValidateUri( text ) );
+        }
     }
 }
