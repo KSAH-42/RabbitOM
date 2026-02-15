dfe60005a985e36f09fdba29cf9c91952540bf51
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-02-15 01:45:59 +0100

    adding a new validator and tests

---
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/Validation/RtspHeaderValidator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers.Validation
+{
+    public static class RtspHeaderValidator
+    {
+        private static readonly Regex AbsoluteUriExpression = new Regex( @"^[A-Za-z][A-Za-z0-9+\-\.]*:[^\s]+$", RegexOptions.Compiled | RegexOptions.CultureInvariant );
+
+        private static readonly Regex RelativeUriExpression = new Regex( @"^(?:\.\.?/|/|[A-Za-z0-9_\-\.~%]+(?:/[A-Za-z0-9_\-\.~%]*)*|[?].+|#.+)$", RegexOptions.Compiled | RegexOptions.CultureInvariant );
+        
+        private static readonly IReadOnlyCollection<char> ForbiddenTokenChars = new HashSet<char>() { ' ' , '(' , ')' , '[' , ']' , '{' ,  '}' , '<' , '>' , ',' , ';' , ':' , '=' , '?' , '¨' , '¤' , 'é' , 'è' , 'à' , 'ù' , 'ç' };
+
+
+
+
+        public static bool TryValidateString( string value )
+        {
+            if ( string.IsNullOrWhiteSpace( value ) )
+            {
+                return false;
+            }
+
+            if ( value.Any( character => character <= 31 || character >= 127  ) )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TryValidateToken( string value )
+        {
+            if ( string.IsNullOrWhiteSpace( value ) )
+            {
+                return false;
+            }
+
+            if ( value.Any( character => character <= 31 || character >= 127 || ForbiddenTokenChars.Contains( character ) ) )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TryValidateUri( string value )
+        {
+            if ( string.IsNullOrWhiteSpace( value ) )
+            {
+                return false;
+            }
+
+            var input = value.Trim();
+
+            if ( value.Any( character => character <= 31 || character >= 127  ) )
+            {
+                return false;
+            }
+
+            if ( AbsoluteUriExpression.IsMatch( input ) )
+            {
+                return Uri.TryCreate(input, UriKind.Absolute, out var uri ) && ! string.IsNullOrWhiteSpace( uri.Scheme );
+            }
+
+            if ( RelativeUriExpression.IsMatch( input ) )
+            {
+                return Uri.TryCreate(input, UriKind.Relative , out var uri ) && ! uri.IsAbsoluteUri;
+            }
+            
+            return false;
+        }
+
+        public static bool TryValidateLong( string value )
+        {
+            return long.TryParse( value , out var number );
+        }
+    }
+}
---
+++ RabbitOM.Tests/Rtsp/Headers/Validation/HeaderValidatorTestSucceed.cs
@@ -0,0 +1,105 @@
+﻿using RabbitOM.Streaming.Experimentation.Rtsp.Headers.Validation;
+using NUnit.Framework;
+
+namespace RabbitOM.Streaming.Tests.Rtsp.Headers.Validation
+{
+    [TestFixture]
+    public class HeaderValidatorTestSucceed
+    {
+        [TestCase( "*" )]
+        [TestCase( "!")]
+        [TestCase( "#")]
+        [TestCase( "$")]
+        [TestCase( "%")]
+        [TestCase( "&")]
+        [TestCase( "'")]
+        [TestCase( "*")]
+        [TestCase( "+")]
+        [TestCase( "-")]
+        [TestCase( ".")]
+        [TestCase( "^")]
+        [TestCase( "_")]
+        [TestCase( "`")]
+        [TestCase( "|")]
+        [TestCase( "~")]
+        [TestCase("(")]
+        [TestCase(")")]
+        [TestCase("[")]
+        [TestCase("]")]
+        [TestCase("{")]
+        [TestCase("}")]
+        [TestCase("<")]
+        [TestCase(">")]
+        [TestCase("@")]
+        [TestCase(",")]
+        [TestCase(";")]
+        [TestCase(":")]
+        [TestCase("=")]
+        [TestCase("?")]
+        [TestCase("/")]
+        [TestCase("\\")]
+        [TestCase( "abcdef" )]
+        [TestCase( "abcdef123" )]
+        [TestCase( "AZERTYUIOPQSDFGHJKLMWXCVBN0123456789" )]
+        [TestCase( " AZERTYUIOPQSDFGH JKLMWXCVBN0123456789 " )]
+        [TestCase( "kader-sahnine" )]
+        [TestCase( "kader_sahnine" )]
+        [TestCase("rtsp://admin:camera123@127.0.0.1:554/screenlive")]
+        [TestCase("session=123;timeout=56")]
+        [TestCase("Digest username=\"admin\";nonce=\"a1a1a1a11a1a1\";")]
+        [TestCase("Digest username='admin';nonce='a1a1a1a11a1a1';")]
+        public void TryValidateStringTestSuccceed( string text )
+        {
+            Assert.IsTrue( RtspHeaderValidator.TryValidateString( text ) );
+        }
+
+        [TestCase( "*" )]
+        [TestCase( "!")]
+        [TestCase( "#")]
+        [TestCase( "$")]
+        [TestCase( "%")]
+        [TestCase( "&")]
+        [TestCase( "'")]
+        [TestCase( "*")]
+        [TestCase( "+")]
+        [TestCase( "-")]
+        [TestCase( ".")]
+        [TestCase( "^")]
+        [TestCase( "_")]
+        [TestCase( "`")]
+        [TestCase( "|")]
+        [TestCase( "~")]
+        [TestCase("/")]
+        [TestCase("\\")]
+        [TestCase( "abcdef" )]
+        [TestCase( "abcdef123" )]
+        [TestCase( "AZERTYUIOPQSDFGHJKLMWXCVBN0123456789" )]
+        [TestCase( "kader-sahnine" )]
+        [TestCase( "kader_sahnine" )]
+        public void TryValidateTokenTestSuccceed( string text )
+        {
+            Assert.IsTrue( RtspHeaderValidator.TryValidateToken( text ) );
+        }
+
+        [TestCase("rtsp://127.0.0.1")]
+        [TestCase("rtsp://127.0.0.1/screenlive")]
+        [TestCase("rtsp://127.0.0.1/screenlive?parameter=1")]
+        [TestCase("rtsp://127.0.0.1/screenlive?parameter=1&parameter2=2")]
+        [TestCase("rtsp://127.0.0.1:554/screenlive")]
+        [TestCase("rtsp://127.0.0.1:554/screenlive?parameter=1&parameter2=2")]
+        [TestCase("rtsp://admin@127.0.0.1")]
+        [TestCase("rtsp://admin@127.0.0.1/screenlive")]
+        [TestCase("rtsp://admin:@127.0.0.1/screenlive")]
+        [TestCase("rtsp://admin:camera123@127.0.0.1/screenlive")]
+        [TestCase("rtsp://admin@127.0.0.1:554/screenlive")]
+        [TestCase("rtsp://admin:@127.0.0.1:554/screenlive")]
+        [TestCase("rtsp://admin:camera123@127.0.0.1:554/screenlive")]
+        [TestCase("rtsp://admin:camera123@127.0.0.1:554/screenlive?a=1&b=2")]
+        [TestCase("abcdef")]
+        [TestCase("abcdef/ght")]
+        public void TryValidateUriTestSucceed( string text )
+        {
+            Assert.IsTrue( RtspHeaderValidator.TryValidateUri( text ) );
+        }
+    }
+}
