b9faddbf4adb8ddf76bd846691d2399a341c41d6
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-02-18 23:58:51 +0100

    enrich a test

---
+++ RabbitOM.Tests/Rtsp/Headers/CSeqRtspHeaderTest.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using RabbitOM.Streaming.Experimentation.Rtsp.Headers;
+using System;
+
+namespace RabbitOM.Streaming.Tests.Rtsp.Headers
+{
+    [TestFixture]
+    public class CSeqRtspHeaderTest
+    {
+        [TestCase( "0" , 0 )]
+        [TestCase( "1" , 1 )]
+        [TestCase( "2" , 2 )]
+        [TestCase( "9223372036854775807" , 9223372036854775807 )]
+         
+        [TestCase( " 0 " , 0 )]
+        [TestCase( " 1 " , 1 )]
+        [TestCase( " 2 " , 2 )]
+        [TestCase( " 9223372036854775807 " , 9223372036854775807 )]
+
+        [TestCase( " '0' " , 0 )]
+        [TestCase( " '1' " , 1 )]
+        [TestCase( " '2' " , 2 )]
+        [TestCase( " '9223372036854775807' " , 9223372036854775807 )]
+
+        [TestCase( " \"0\" " , 0 )]
+        [TestCase( " \"1\" " , 1 )]
+        [TestCase( " \"2\" " , 2 )]
+        [TestCase( " \"9223372036854775807\" " , 9223372036854775807 )]
+
+        [TestCase( " \"-9223372036854775808\"" , -9223372036854775808 )]
+        [TestCase( " \"-9223372036854775808" , -9223372036854775808 )]
+        [TestCase( " '-9223372036854775808" , -9223372036854775808 )]
+
+        public void CheckParseSucceed( string input , long expectedId )
+        {
+            Assert.IsTrue( CSeqRtspHeader.TryParse( input , out var header ) );
+            Assert.IsNotNull( header );
+            Assert.AreEqual( expectedId , header.Value );
+        }
+
+        [TestCase( null ) ]
+        [TestCase( "" ) ]
+        [TestCase( " " ) ]
+        [TestCase( "a" ) ]
+        [TestCase( "aa" ) ]
+        [TestCase( " aa " ) ]
+        [TestCase( " a1a " ) ]
+        [TestCase( " ? " ) ]
+        [TestCase( " '' " ) ]
+        [TestCase( "''" ) ]
+        [TestCase( "\"\"" ) ]
+        [TestCase( "\" \"" ) ]
+        [TestCase( " \" \" " ) ]
+        public void CheckParseFailed( string input  )
+        {
+            Assert.IsFalse( CSeqRtspHeader.TryParse( input , out var header ) );
+            Assert.IsNull( header );
+        }
+
+
+        [Test]
+        public void CheckToString()
+        {
+            var header = new CSeqRtspHeader();
+
+            Assert.AreEqual( 0 , header.Value );
+            Assert.AreEqual( "0" , header.ToString() );
+
+            header.Value = 0;
+            Assert.AreEqual( "0" , header.ToString() );
+
+            header.Value = 1;
+            Assert.AreEqual( "1" , header.ToString() );
+
+            header.Value = long.MaxValue;
+            Assert.AreEqual( long.MaxValue.ToString() , header.ToString() );
+        }
+    }
+}
