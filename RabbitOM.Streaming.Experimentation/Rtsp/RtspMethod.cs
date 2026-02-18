39a9f5ae11c222ea56b09df10378ad6b19805483
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-02-18 18:23:51 +0100

    changing member order

--- RabbitOM.Streaming.Experimentation/Rtsp/RtspMethod.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/RtspMethod.cs
@@ -7,27 +7,39 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp
     {
         private RtspMethod( string name ) => Name = name;
         
-
-
-
         public string Name { get; }
        
+        public override string ToString() => Name;
+
+
+
 
 
 
         public static RtspMethod OPTIONS { get; }  =new RtspMethod( "OPTIONS" );
+        
         public static RtspMethod DESCRIBE { get; } = new RtspMethod( "DESCRIBE" );
+        
         public static RtspMethod SETUP { get; } = new RtspMethod( "SETUP" );
+        
+        
         public static RtspMethod PLAY { get; } = new RtspMethod( "PLAY" );
+        
         public static RtspMethod PAUSE { get; } = new RtspMethod( "PAUSE" );
+        
         public static RtspMethod TEARDOWN { get; } = new RtspMethod( "TEARDOWN" );
+        
         public static RtspMethod KEEPALIVE { get; } = new RtspMethod( "KEEPALIVE" );
+        
         public static RtspMethod GET_PARAMETER { get; } = new RtspMethod( "GET_PARAMETER" );
+        
         public static RtspMethod SET_PARAMETER { get; } = new RtspMethod( "SET_PARAMETER" );
+        
         public static RtspMethod ANNOUNCE { get; } = new RtspMethod( "ANNOUNCE" );
+        
         public static RtspMethod REDIRECT { get; } = new RtspMethod( "REDIRECT" );
+        
         public static RtspMethod RECORD { get; } = new RtspMethod( "RECORD" );
-
 
 
 
@@ -64,10 +76,5 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp
 
             return result != null;
         }
-
-
-
-
-        public override string ToString() => Name;
     }
 }
