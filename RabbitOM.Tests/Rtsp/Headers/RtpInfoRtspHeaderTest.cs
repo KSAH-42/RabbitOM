834f1f14202dc33ce04d3fa1a7edd1b18b667bb7
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-02-24 12:31:17 +0100

    adding new tests

---
+++ RabbitOM.Tests/Rtsp/Headers/RtpInfoRtspHeaderTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using RabbitOM.Streaming.Experimentation.Rtsp;
+using RabbitOM.Streaming.Experimentation.Rtsp.Headers;
+using RabbitOM.Streaming.Experimentation.Rtsp.Headers.Types;
+using System;
+using System.Linq;
+
+namespace RabbitOM.Streaming.Tests.Rtsp.Headers.Types
+{
+    [TestFixture]
+    public class RtpInfoRtspHeaderTest
+    {
+        [TestCase( "url=rtsp://127.0.0.1;seq=123;rtptime=321;ssrc=abcdef" , 1 )]
+        [TestCase( "url=rtsp://127.0.0.1;seq=123;rtptime=321;ssrc=abcdef," , 1 )]
+        [TestCase( "url=rtsp://127.0.0.1;seq=123;rtptime=321;ssrc=abcdef, url=rtsp://127.0.0.1;seq=123;rtptime=321;ssrc=abcdef " , 2 )]
+        [TestCase( "url=rtsp://127.0.0.1;seq=123;rtptime=321;ssrc=abcdef, url=rtsp://127.0.0.1;seq=123;rtptime=321;ssrc=abcdef , url=rtsp://127.0.0.1;seq=123;rtptime=321;ssrc=abcdef " , 3 )]
+        public void CheckParseSucceed(string input , int count )
+        {
+            Assert.IsTrue( RtpInfoRtspHeader.TryParse( input , out var header ) );
+            Assert.AreEqual( count , header.Infos.Count );
+        }
+
+        [TestCase( null ) ]
+        [TestCase( "" ) ]
+        [TestCase( " " ) ]
+        [TestCase( "," ) ]
+        [TestCase( ",,,,," ) ]
+        [TestCase( "rul=rtsp://127.0.0.1;seq=a" ) ]
+        public void CheckParseFailed(string input )
+        {
+            Assert.IsFalse( RtpInfoRtspHeader.TryParse( input , out var header ) );
+            Assert.IsNull( header );
+        }
+
+    }
+}
