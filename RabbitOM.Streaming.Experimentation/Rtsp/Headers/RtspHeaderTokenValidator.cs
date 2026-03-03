7e866cbc047ba8fa13c31873c5aeb699acc09bed
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-04 00:15:19 +0100

    adding refactorization comment

--- RabbitOM.Streaming.Experimentation/Rtsp/Headers/RtspHeaderTokenValidator.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/RtspHeaderTokenValidator.cs
@@ -3,6 +3,8 @@ using System.Linq;
 
 namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers
 {
+    // TODO: does header must contains it's own token validation an expose a public static method called IsValidToken ?
+    // or used centralized approach component even if there is some duplicated code in some headers ?
     internal sealed class RtspHeaderTokenValidator
     {
         public bool ContainsLetterOrDigit( string value )
