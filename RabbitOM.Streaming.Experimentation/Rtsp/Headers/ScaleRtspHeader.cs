7873c1df2632866adc0808480be8b2149f383352
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-09 12:50:07 +0100

    rearrange the file

--- RabbitOM.Streaming.Experimentation/Rtsp/Headers/ScaleRtspHeader.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/ScaleRtspHeader.cs
@@ -10,19 +10,16 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers
         
         public float Value { get; set; }
 
+        public static bool TryParse( string input , out ScaleRtspHeader result )
+        {
+            result = float.TryParse( StringValueAdapter.TrimWithUnQuoteAdapter.Adapt( input ).Replace( "," , "." ) , NumberStyles.Float , CultureInfo.InvariantCulture , out var value ) ? new ScaleRtspHeader() { Value = value } : null ;
+
+            return result != null;
+        }
+
         public override string ToString()
         {
             return Value.ToString( "G2" , CultureInfo.InvariantCulture );
-        }
-
-        public static bool TryParse( string input , out ScaleRtspHeader result )
-        {
-            result = float.TryParse( StringValueAdapter.TrimWithUnQuoteAdapter.Adapt( input ).Replace( "," , "." ) , NumberStyles.Float , CultureInfo.InvariantCulture , out var value )
-                    ? new ScaleRtspHeader() { Value = value } 
-                    : null
-                    ;
-
-            return result != null;
         }
     }
 }
