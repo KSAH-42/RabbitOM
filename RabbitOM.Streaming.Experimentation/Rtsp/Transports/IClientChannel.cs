62740bc9a018d034bf96d1b5f95fc3b0691bd1d5
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-13 15:20:09 +0100

    adding the abort method

--- RabbitOM.Streaming.Experimentation/Rtsp/Transports/IClientChannel.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/IClientChannel.cs
@@ -19,6 +19,8 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports
         
         void Close();
 
+        void Abort();
+
         RtspResponseMessage SendMessage( RtspRequestMessage request );
     }
 }
