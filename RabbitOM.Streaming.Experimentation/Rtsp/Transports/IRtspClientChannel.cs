63b77f1df6a5980f6907c1db792a69f988bd994a
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-13 13:29:14 +0100

    adding the client channel

---
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/IRtspClientChannel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports
+{
+
+    public interface IRtspClientChannel : IDisposable
+    {        
+        string EndPoint { get; set; }
+        
+        TimeSpan ReceiveTimeout { get; set; }
+        
+        TimeSpan SendTimeout { get; set; }
+
+        bool IsOpened { get; }
+
+
+
+
+        void Open();
+        
+        void Close();
+
+        RtspResponseMessage SendMessage( RtspRequestMessage request );
+    }
+}
