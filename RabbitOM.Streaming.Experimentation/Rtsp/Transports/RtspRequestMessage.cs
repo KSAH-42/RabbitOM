557130b5c0b71aaabf791565d3a13e4485cd3e4f
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-13 15:15:36 +0100

    create messaging class

--- RabbitOM.Streaming.Experimentation/Rtsp/Transports/RtspRequestMessage.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/RtspRequestMessage.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports
 {
-
     public sealed class RtspRequestMessage
     {        
+        public string Method { get; set; }
+
+        public string Version { get; set; }
+
+        public string Uri { get; set; }
+
+        public IDictionary<string,string> Headers { get; } = new Dictionary<string,string>( StringComparer.OrdinalIgnoreCase );
+
+        public byte[] Body { get; set; }
+
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryParse( string input , out RtspResponseMessage result )
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- RabbitOM.Streaming.Experimentation/Rtsp/Transports/RtspResponseMessage.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/RtspResponseMessage.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports
 {
-
     public sealed class RtspResponseMessage
     {        
+        public string Method { get; set; }
+        
+        public string Version { get; set; }
+        
+        public string ReasonPhase { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public IDictionary<string,string> Headers { get; } = new Dictionary<string,string>( StringComparer.OrdinalIgnoreCase );
+
+        public byte[] Body { get; set; }
+
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TryParse( string input , out RtspResponseMessage result )
+        {
+            throw new NotImplementedException();
+        }
     }
 }
