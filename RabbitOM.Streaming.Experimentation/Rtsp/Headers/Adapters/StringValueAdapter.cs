dbacf9d54bed1725da713072e902b2cdaf750f28
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-23 14:59:33 +0100

    adding a new adapter

--- RabbitOM.Streaming.Experimentation/Rtsp/Headers/Adapters/StringValueAdapter.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/Adapters/StringValueAdapter.cs
@@ -8,6 +8,7 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers.Adapters
         public static StringValueAdapter TrimWithUnQuoteAdapter { get; } = new TrimWithUnQuoteValueAdapter();
         public static StringValueAdapter TrimWithSuppressQuoteAdapter { get; } = new TrimWithSuppressQuoteValueAdapter();
         public static StringValueAdapter QuoteAdapter { get; } = new QuoteValueAdapter();
+        public static StringValueAdapter UnQuoteAdapter { get; } = new UnQuoteValueAdapter();
 
 
         public abstract string Adapt( string value );
---
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/Adapters/UnQuoteValueAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers.Adapters
+{
+    public sealed class UnQuoteValueAdapter : StringValueAdapter
+    {
+        private static readonly char[] SpaceAndQuotesChars = { '\'' , '\"' , '`' };
+
+        public override string Adapt( string value )
+        {
+            return value?.Trim( SpaceAndQuotesChars ) ?? string.Empty;
+        }
+    }
+}
