d35ad1aabf9166b3d8a184bf0752c7fca5ac8890
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-24 00:19:07 +0100

    removing a using directive

--- RabbitOM.Streaming.Experimentation/Rtsp/Headers/Types/SupportedTypes.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/Types/SupportedTypes.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers.Types
 {
