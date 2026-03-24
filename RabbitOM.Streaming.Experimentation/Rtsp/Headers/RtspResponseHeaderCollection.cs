5a078fa97493b827e52ccfa375bf69b997ed3242
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-24 09:40:36 +0100

    implement response headers collection

--- RabbitOM.Streaming.Experimentation/Rtsp/Headers/RtspResponseHeaderCollection.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/RtspResponseHeaderCollection.cs
@@ -2,46 +2,119 @@
 
 namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers
 {
-    // TODO: on getter, implement and use the method ElementOrDefaultAt( string name ) and the method ElementOrDefaultAt( string name , int index )
-    // TODO: on setter do not throw exception on null, prefer removing from the internal collection
     public class RtspResponseHeaderCollection : RtspHeaderCollection
     {
-        public AllowRtspHeaderValue Allow { get; set; }
+        public CSeqRtspHeaderValue CSeq
+        {
+            get => GetValue<CSeqRtspHeaderValue>( CSeqRtspHeaderValue.TypeName , () => new CSeqRtspHeaderValue() );
+        }
 
-        public AuthenticateRtspHeaderValue Authenticate { get; set; }
+        public AllowRtspHeaderValue Allow
+        {
+            get => GetValue<AllowRtspHeaderValue>( AllowRtspHeaderValue.TypeName );
+            set => SetValue<AllowRtspHeaderValue>( AllowRtspHeaderValue.TypeName , value );
+        }
 
-        public CacheControlRtspHeaderValue CacheControl { get; set; }
-        
-        public ConferenceRtspHeaderValue Conference { get; set; }
-        
-        public ConnectionRtspHeaderValue Connection { get; set; }
-        
-        public ContentBaseRtspHeaderValue ContentBase { get; set; }
-        
-        public ContentRangeRtspHeaderValue ContentRange { get; set; }
-        
-        public CSeqRtspHeaderValue CSeq { get; } = new CSeqRtspHeaderValue();
-        
-        public DateRtspHeaderValue Date { get; set; }
-        
-        public ExpiresRtspHeaderValue Expires { get; set; }
+        public AuthenticateRtspHeaderValue Authenticate
+        {
+            get => GetValue<AuthenticateRtspHeaderValue>( AuthenticateRtspHeaderValue.TypeName );
+            set => SetValue<AuthenticateRtspHeaderValue>( AuthenticateRtspHeaderValue.TypeName , value );
+        }
 
-        public LastModifiedRtspHeaderValue LastModified { get; set; }
+        public CacheControlRtspHeaderValue CacheControl
+        {
+            get => GetValue<CacheControlRtspHeaderValue>( CacheControlRtspHeaderValue.TypeName );
+            set => SetValue<CacheControlRtspHeaderValue>( CacheControlRtspHeaderValue.TypeName , value );
+        }
         
-        public LocationRtspHeaderValue Location { get; set; }
+        public ConferenceRtspHeaderValue Conference
+        {
+            get => GetValue<ConferenceRtspHeaderValue>( ConferenceRtspHeaderValue.TypeName );
+            set => SetValue<ConferenceRtspHeaderValue>( ConferenceRtspHeaderValue.TypeName , value );
+        }
         
-        public MediaDurationRtspHeaderValue MediaDuration { get; set; }
+        public ConnectionRtspHeaderValue Connection
+        {
+            get => GetValue<ConnectionRtspHeaderValue>( ConnectionRtspHeaderValue.TypeName );
+            set => SetValue<ConnectionRtspHeaderValue>( ConnectionRtspHeaderValue.TypeName , value );
+        }
         
-        public PublicRtspHeaderValue Public { get; set; }
+        public ContentBaseRtspHeaderValue ContentBase
+        {
+            get => GetValue<ContentBaseRtspHeaderValue>( ContentBaseRtspHeaderValue.TypeName );
+            set => SetValue<ContentBaseRtspHeaderValue>( ContentBaseRtspHeaderValue.TypeName , value );
+        }
         
-        public RtpInfoRtspHeaderValue RtpInfo { get; set; }
+        public ContentRangeRtspHeaderValue ContentRange
+        {
+            get => GetValue<ContentRangeRtspHeaderValue>( ContentRangeRtspHeaderValue.TypeName );
+            set => SetValue<ContentRangeRtspHeaderValue>( ContentRangeRtspHeaderValue.TypeName , value );
+        }
         
-        public SessionRtspHeaderValue Session { get; set; }
+        public DateRtspHeaderValue Date
+        {
+            get => GetValue<DateRtspHeaderValue>( DateRtspHeaderValue.TypeName );
+            set => SetValue<DateRtspHeaderValue>( DateRtspHeaderValue.TypeName , value );
+        }
         
-        public TransportRtspHeaderValue Transport { get; set; }
-        
-        public ViaRtspHeaderValue Via { get; set; }
+        public ExpiresRtspHeaderValue Expires
+        {
+            get => GetValue<ExpiresRtspHeaderValue>( ExpiresRtspHeaderValue.TypeName );
+            set => SetValue<ExpiresRtspHeaderValue>( ExpiresRtspHeaderValue.TypeName , value );
+        }
 
-        public WarningRtspHeaderValue Warning { get; set; }
+        public LastModifiedRtspHeaderValue LastModified
+        {
+            get => GetValue<LastModifiedRtspHeaderValue>( LastModifiedRtspHeaderValue.TypeName );
+            set => SetValue<LastModifiedRtspHeaderValue>( LastModifiedRtspHeaderValue.TypeName , value );
+        }
+        
+        public LocationRtspHeaderValue Location
+        {
+            get => GetValue<LocationRtspHeaderValue>( LocationRtspHeaderValue.TypeName );
+            set => SetValue<LocationRtspHeaderValue>( LocationRtspHeaderValue.TypeName , value );
+        }
+        
+        public MediaDurationRtspHeaderValue MediaDuration
+        {
+            get => GetValue<MediaDurationRtspHeaderValue>( MediaDurationRtspHeaderValue.TypeName );
+            set => SetValue<MediaDurationRtspHeaderValue>( MediaDurationRtspHeaderValue.TypeName , value );
+        }
+        
+        public PublicRtspHeaderValue Public
+        {
+            get => GetValue<PublicRtspHeaderValue>( PublicRtspHeaderValue.TypeName );
+            set => SetValue<PublicRtspHeaderValue>( PublicRtspHeaderValue.TypeName , value );
+        }
+        
+        public RtpInfoRtspHeaderValue RtpInfo
+        {
+            get => GetValue<RtpInfoRtspHeaderValue>( RtpInfoRtspHeaderValue.TypeName );
+            set => SetValue<RtpInfoRtspHeaderValue>( RtpInfoRtspHeaderValue.TypeName , value );
+        }
+        
+        public SessionRtspHeaderValue Session
+        {
+            get => GetValue<SessionRtspHeaderValue>( SessionRtspHeaderValue.TypeName );
+            set => SetValue<SessionRtspHeaderValue>( SessionRtspHeaderValue.TypeName , value );
+        }
+        
+        public TransportRtspHeaderValue Transport
+        {
+            get => GetValue<TransportRtspHeaderValue>( TransportRtspHeaderValue.TypeName );
+            set => SetValue<TransportRtspHeaderValue>( TransportRtspHeaderValue.TypeName , value );
+        }
+        
+        public ViaRtspHeaderValue Via
+        {
+            get => GetValue<ViaRtspHeaderValue>( ViaRtspHeaderValue.TypeName );
+            set => SetValue<ViaRtspHeaderValue>( ViaRtspHeaderValue.TypeName , value );
+        }
+
+        public WarningRtspHeaderValue Warning
+        {
+            get => GetValue<WarningRtspHeaderValue>( WarningRtspHeaderValue.TypeName );
+            set => SetValue<WarningRtspHeaderValue>( WarningRtspHeaderValue.TypeName , value );
+        }
     }
 }
