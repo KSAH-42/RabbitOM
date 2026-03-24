f43b8943da7d824246651fa7fbcb68285fbee037
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-03-24 15:58:31 +0100

    refactor

--- RabbitOM.Streaming.Experimentation/Rtsp/Headers/StringRtspHeaderValue.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Headers/StringRtspHeaderValue.cs
@@ -4,70 +4,22 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Headers
 {
     public sealed class StringRtspHeaderValue : RtspHeaderValue
     {
-        private string _value = string.Empty;
+        public StringRtspHeaderValue( string value ) => Value = value?.Trim() ?? string.Empty;
 
 
+        public string Value { get; }
 
-
-
-        public StringRtspHeaderValue()
-        {
-        }
-
-        public StringRtspHeaderValue( string value )
-        {
-            Value = value;
-        }
-
-
-
-
-
-
-
-
-        public string Value
-        {
-            get => _value;
-            set => _value = value?.Trim() ?? string.Empty;
-        }
-
-
-
-
-
-
-
-
-
-        public static implicit operator StringRtspHeaderValue( string value )
-        {
-            return new StringRtspHeaderValue( value );
-        }
-
+        
+        public static implicit operator StringRtspHeaderValue( string value ) => new StringRtspHeaderValue( value );
+        
         public static bool TryParse( string input , out StringRtspHeaderValue result )
         {
-            result = null;
-
-            if ( string.IsNullOrWhiteSpace( input ) )
-            {
-                return false;
-            }
-
-            result = new StringRtspHeaderValue( input );
-
-            return true;
+            result = string.IsNullOrWhiteSpace( input ) ? null : new StringRtspHeaderValue( input );
+            
+            return result != null;
         }
 
 
-
-
-
-
-
-        public override string ToString()
-        {
-            return Value;
-        }
+        public override string ToString() => Value;
     }
 }
