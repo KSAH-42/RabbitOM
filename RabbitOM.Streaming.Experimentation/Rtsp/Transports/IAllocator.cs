821b8dc1262ee0bfa57d9049625f2092657897ee
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-05-14 21:20:43 +0200

    renaming methods

--- RabbitOM.Streaming.Experimentation/Rtsp/Transports/IAllocator.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/IAllocator.cs
@@ -4,8 +4,8 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports
 {
     public interface IAllocator : IDisposable
     {
-        byte[] AllocateBuffer();
-
-        byte[] AllocateBuffer( int size );
+        byte[] Rent( int size );
+        
+        void Return( byte[] buffer );
     }
 }
