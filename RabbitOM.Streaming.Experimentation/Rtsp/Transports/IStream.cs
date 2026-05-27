c8d82cbd3cc197aa064388bfb22ad031281d5de8
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-05-27 08:31:28 +0200

    add comments

--- RabbitOM.Streaming.Experimentation/Rtsp/Transports/IStream.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/IStream.cs
@@ -9,6 +9,16 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports
     // for instance any read on the stream will use an internal large buffer
     // without to increment the fully position, only when the count bytes use to read,
     // is just smaller than the size of the internal buffer.
+    
+    //     / \
+    //    / | \
+    //   /  |  \
+    //  /   *   \
+    // /_________\
+
+    // an interface has been used instead of using the base .net class Stream
+    // the reasons comes that the Stream class expose seeks methods that
+    // can introduce confusion
 
     public interface IStream : IDisposable
     {
@@ -33,48 +43,4 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports
 
         void Close();
     }
-
-    //public abstract class IStream : IDisposable
-    //{
-    //    ~IStream()
-    //    {
-    //    }
-
-
-
-    //    public abstract bool CanRead { get; }
-        
-    //    public abstract bool CanWrite { get; }
-
-
-
-        
-    //    public abstract string ReadLine();
-
-    //    public abstract int PeekByte();
-
-    //    public abstract int ReadByte();
-
-    //    public abstract int Read( byte[] buffer , int offset , int count );
-
-    //    public abstract int WriteByte( byte value );
-
-    //    public abstract int Write( byte[] buffer , int offset , int count );
-
-    //    public abstract void Close();
-
-    //    public void Dispose()
-    //    {
-    //        Dispose( true );
-    //        GC.SuppressFinalize( this );
-    //    }
-
-    //    protected virtual void Dispose(bool disposing)
-    //    {
-    //        if ( disposing )
-    //        {
-    //            Close();
-    //        }
-    //    }
-    //}
 }
