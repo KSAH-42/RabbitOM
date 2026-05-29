e24c276635236722ccb793cd6190f55e1c598dc5
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-05-29 03:18:50 +0200

    adding the rtsp listener/server

---
+++ RabbitOM.Streaming.Experimentation/Rtsp/IListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Experimentation.Rtsp
+{
+    /// <summary>
+    /// Represent the rtsp listener for receiving rtsp client request
+    /// </summary>
+    public interface IListener
+    {
+        event EventHandler Started;
+
+        event EventHandler Stopped;
+
+
+        bool IsStarted { get; }
+
+
+        void Start();
+
+        void Stop();
+    }
+}
