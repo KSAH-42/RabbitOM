669b32363ac6caa2e1d249233d0cf7f9c49cab5d
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-05-31 21:16:48 +0200

    for perf improvement, implement the parse in different way

--- RabbitOM.Streaming.Experimentation/Rtsp/Transports/Channels/RtspStatusLine.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/Channels/RtspStatusLine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports.Channels
 {
@@ -12,14 +13,93 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports.Channels
 
         public string Reason { get; set; }
 
+
+
+
+
+
+
         public static bool TryParse( string input , out RtspStatusLine result )
         {
-            throw new NotImplementedException();
+            result = null;
+
+            if ( string.IsNullOrEmpty( input ) )
+            {
+                return false;
+            }
+
+            // here we don't use string.split, the result show signatificative improvement 
+
+            // RTSP / 1.0 555 Doom-Patrol
+
+            var statusLine = new RtspStatusLine();
+            var builder = new StringBuilder();
+            
+            for ( var i = 0 ; i < input.Length ; ++ i )
+            {
+                var element = input[ i ];
+
+                if ( element == ' ' || element == '/' && statusLine.Version == null )
+                {
+                    if ( builder.Length > 0 )
+                    {
+                        Populate( statusLine , builder.ToString() );
+
+                        builder.Clear();
+                    }
+                }
+                else
+                {
+                    builder.Append( element );
+                }
+            }
+
+            if ( builder.Length > 0 )
+            {
+                Populate( statusLine , builder.ToString() );
+
+                builder.Clear();
+            }
+
+            if ( string.IsNullOrEmpty( statusLine.Protocol ) || string.IsNullOrEmpty( statusLine.Version ) || string.IsNullOrEmpty( statusLine.Code ) )
+            {
+                return false;
+            }
+
+            result = statusLine;
+
+            return true;
         }
 
+        private static void Populate( RtspStatusLine line , string token )
+        {
+            if ( line.Protocol == null )
+            {
+                line.Protocol = token;
+            }
+            else if ( line.Version == null )
+            {
+                line.Version = token;
+            }
+            else if ( line.Code == null )
+            {
+                line.Code = token;
+            }
+            else if ( line.Reason == null )
+            {
+                line.Reason = token;
+            }
+        }
+
+
+
+
+
+
+        // we are at the low level, do not add null empty checks
         public override string ToString()
         {
-            throw new NotImplementedException();
+            return $"{Protocol}/{Version} {Code} {Reason}";
         }
     }
 }
