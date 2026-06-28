476f1b1267a4d6e88ba678bd57fe237dad29e707
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-06-28 02:31:19 +0200

    introducing a request pipeline

---
+++ RabbitOM.Streaming.Experimentation/Rtsp/IRequestPipeLine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.Experimentation.Rtsp
+{
+    // Introduce an incomplete pipeline interface, it should be review in depth
+    public interface IRequestPipeLine
+    {
+        RtspClientResponse Execute( RtspMethod method , string uri , RtspClientRequest request );
+    }
+}
