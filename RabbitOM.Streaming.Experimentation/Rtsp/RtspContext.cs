3e798cecc4e7305d84de8d49c5663213bfa0fac4
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-06-30 12:13:00 +0200

    adding a property

--- RabbitOM.Streaming.Experimentation/Rtsp/RtspContext.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/RtspContext.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RabbitOM.Streaming.Experimentation.Rtsp
 {
     public sealed class RtspContext
     {
+        private Dictionary<string,object> _parameters;
+
         public RtspContext( string method , string uri )
         {
             Method = method;
@@ -13,5 +16,18 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp
         public string Method { get; }
 
         public string Uri { get; }
+
+        public Dictionary<string,object> Parameters
+        {
+            get
+            {
+                if ( _parameters == null )
+                {
+                    _parameters = new Dictionary<string, object>( StringComparer.OrdinalIgnoreCase );
+                }
+
+                return _parameters;
+            }
+        }
     }
 }
