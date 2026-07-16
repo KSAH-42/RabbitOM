a31fb1052b4f7294dd2e566b48b444599ea87875
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-07-16 22:44:00 +0200

    minor changes

--- RabbitOM.Streaming.Experimentation/Rtsp/Transports/Channels/RtspRequester.cs
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/Channels/RtspRequester.cs
@@ -8,14 +8,14 @@ namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports.Channels
 
     public sealed class RtspRequester
     {
-        public async Task SendMessageAsync( RtspInterleavedMessage interleavedData , CancellationToken cancellationToken )
+        public Task SendMessageAsync( RtspInterleavedMessage interleavedData , CancellationToken cancellationToken )
         {
             throw new NotImplementedException();
         }
 
         // TODO: returns and let the higher level to raise an event handler for the message received
 
-        public async Task<RtspResponseMessage> SendMessageAsync( RtspRequestMessage request , CancellationToken cancellationToken )
+        public Task<RtspResponseMessage> SendMessageAsync( RtspRequestMessage request , CancellationToken cancellationToken )
         {
             throw new NotImplementedException();
         }
