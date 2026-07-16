d41a9261d2da1130fd55e07bb1d3e6459f7faf3f
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-07-16 08:15:40 +0200

    adding the requester class

---
+++ RabbitOM.Streaming.Experimentation/Rtsp/Transports/Channels/RtspRequester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitOM.Streaming.Experimentation.Rtsp.Transports.Channels
+{
+    // TODO: used correlator classes, readers and writers
+
+    public sealed class RtspRequester
+    {
+        public async Task SendMessageAsync( RtspInterleavedMessage interleavedData , CancellationToken cancellationToken )
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: returns and let the higher level to raise an event handler for the message received
+
+        public async Task<RtspResponseMessage> SendMessageAsync( RtspRequestMessage request , CancellationToken cancellationToken )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
