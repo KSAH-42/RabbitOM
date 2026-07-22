5ace801dc066edabfb1fe8a872a605ac699bdd09
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-07-22 04:03:23 +0200

    adding comments reason

--- RabbitOM/Streaming/Rtp/H266/H266FrameBuilder.cs
+++ RabbitOM/Streaming/Rtp/H266/H266FrameBuilder.cs
@@ -9,7 +9,7 @@ namespace RabbitOM.Streaming.Rtp.H266
         public H266FrameBuilder()
         {
 #if !DEBUG
-            throw new NotSupportedException("the implementation is not tested with multiple products and server, and it can not be used until this code will be tested with compliant server");
+            throw new NotSupportedException("I need a camera that support H266, so I break it");
 #endif
         }
 
