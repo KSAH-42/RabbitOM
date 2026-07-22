68abbd267ea135fa0444aeca9608d51094b90351
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-07-22 11:25:38 +0200

    un comment

--- RabbitOM/Streaming/Sdp/BandwithFieldCollection.cs
+++ RabbitOM/Streaming/Sdp/BandwithFieldCollection.cs
@@ -3,24 +3,13 @@ using System.Collections.Generic;
 
 namespace RabbitOM.Streaming.Sdp
 {
-    /// <summary>
-    /// Represent the collection for bandwith field
-    /// </summary>
-    public sealed class BandwithFieldCollection : FieldCollection<BandwithField> 
+    public sealed class BandwithFieldCollection : FieldCollection<BandwithField>
     {
-        /// <summary>
-        /// Initialize a new instance of the collection
-        /// </summary>
         public BandwithFieldCollection()
         {
         }
 
-        /// <summary>
-        /// Initialize a new instance of the collection
-        /// </summary>
-        /// <param name="fields">the collection of field</param>
-        public BandwithFieldCollection(IEnumerable<BandwithField> fields)
-            : base(fields)
+        public BandwithFieldCollection(IEnumerable<BandwithField> fields) : base(fields)
         {
         }
     }
--- RabbitOM/Streaming/Sdp/BaseField.cs
+++ RabbitOM/Streaming/Sdp/BaseField.cs
@@ -2,26 +2,13 @@
 
 namespace RabbitOM.Streaming.Sdp
 {
-    /// <summary>
-    /// Represent a sdp field
-    /// </summary>
-    public abstract class BaseField	
+    public abstract class BaseField
     {
-        /// <summary>
-        /// Gets the type name
-        /// </summary>
         public abstract string TypeName
         {
             get;
         }
 
-
-
-
-        /// <summary>
-        /// Validate
-        /// </summary>
-        /// <exception cref="ValidationException"/>
         public virtual void Validate()
         {
             if ( ! TryValidate() )
@@ -30,16 +17,8 @@ namespace RabbitOM.Streaming.Sdp
             }
         }
 
-        /// <summary>
-        /// Try to validate
-        /// </summary>
-        /// <returns>returns true for a success, otherwise false</returns>
         public abstract bool TryValidate();
 
-        /// <summary>
-        /// Format to a string
-        /// </summary>
-        /// <returns>returns a string</returns>
         public abstract override string ToString();
     }
 }
