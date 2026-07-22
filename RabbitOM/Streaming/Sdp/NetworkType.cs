5daafdf8804fd72d04b0a8506f3de750ced770f0
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-07-22 12:56:30 +0200

    un comment

--- RabbitOM/Streaming/Sdp/NetworkType.cs
+++ RabbitOM/Streaming/Sdp/NetworkType.cs
@@ -1,18 +1,9 @@
 ﻿namespace RabbitOM.Streaming.Sdp
 {
-    /// <summary>
-    /// Represent the network type
-    /// </summary>
     public enum NetworkType
     {
-        /// <summary>
-        /// Unknown
-        /// </summary>
         None = 0,
 
-        /// <summary>
-        /// Internet
-        /// </summary>
         Internet,
     }
 }
--- RabbitOM/Streaming/Sdp/ProtocolType.cs
+++ RabbitOM/Streaming/Sdp/ProtocolType.cs
@@ -1,18 +1,9 @@
 ﻿namespace RabbitOM.Streaming.Sdp
 {
-    /// <summary>
-    /// Represent the protocol type
-    /// </summary>
     public enum ProtocolType
     {
-        /// <summary>
-        /// Unknown
-        /// </summary>
         None = 0,
 
-        /// <summary>
-        /// The realtime Transport Protocol
-        /// </summary>
         RTP,
     }
 }
