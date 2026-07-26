da35d0c6b605e4658cd0213e5c96652fa4501852
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-07-26 13:57:03 +0200

    removing comments

--- RabbitOM.Experimentation/Rtspv2/RtspClientRequestInfo.cs
+++ RabbitOM.Experimentation/Rtspv2/RtspClientRequestInfo.cs
@@ -5,7 +5,6 @@ namespace RabbitOM.Streaming.RtspV2
 {
     using RabbitOM.Streaming.RtspV2.Headers;
 
-    // TODO: rename this class to avoid confusion with the client.Options method 
     public sealed class RtspClientRequestInfo
     {
         public Uri Uri { get; }
