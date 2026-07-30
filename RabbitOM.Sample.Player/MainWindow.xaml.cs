b06963faab3c7b5ecee76b68c1058eebba764320
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-07-31 00:38:32 +0200

    modifing comments about player archictecture

--- RabbitOM.Sample.Player/MainWindow.xaml.cs
+++ RabbitOM.Sample.Player/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace RabbitOM.Sample.Client.Player
     // This is not a clean code here if we respect wpf, and others things, it's juste a demo, refactorization must be done
     // There is a base line here, but the right direction for writing a correct architecture is to write a graph and setup it using a builder
     // just something similar to dsf using a modern approachs
-    // or something different than a multi thread graphs
+    // or something different than a multi thread graph
 
     public partial class MainWindow : Window
     {
