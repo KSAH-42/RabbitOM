d92db6d83aa34992acb82cdbf8a3224b9c5d045a
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-01 03:22:58 +0200

    adding comments about GC and good practices

--- RabbitOM.Sample.Player/Decoders/FFMpeg/FFMpegDecoder.cs
+++ RabbitOM.Sample.Player/Decoders/FFMpeg/FFMpegDecoder.cs
@@ -257,7 +257,7 @@ namespace RabbitOM.Sample.Player.Codecs.FFMpeg
                 var got_frame = 0;
 
                 // normally, in this case, it's highly recommended to set to default _rawPacket->data as null after pin buffer adress and calling ffmpeg.decode func using a try finally bloc, the compactor on the GC can change address of the raw buffer
-                // that's why setting _rawPacket->data = null, at the end is highly recommended. Here i don't do it. the reason comes frame that the packet is only used here, only in this place.
+                // that's why setting _rawPacket->data = null, at the end is highly recommended. Here i don't do it. the reason comes frame that the packet is only used here, only in this place. And using C or C++, a free or even a delete on null pointer make anything.
 	            _rawPacket->data = rawBuffer;
 	            _rawPacket->size = buffer.Length;
 
