bcc4f724c8d0e8c4cac5f199cf07a5d865679d4b
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-01 15:40:14 +0200

    adding a custom style

---
+++ RabbitOM.Sample.Player/Styles/WindowStyle.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RabbitOM.Sample.Player.Styles
+{
+	public partial class WindowStyle : ResourceDictionary
+	{
+		public WindowStyle()
+		{
+			InitializeComponent();
+		}
+
+
+		public void TileBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+			var element = sender as FrameworkElement;
+
+			if ( element == null )
+			{
+				return;
+			}
+
+			var window = element.TemplatedParent as Window;
+
+			window?.DragMove();
+        }
+
+		public void MainBorder_MouseLeftButtonDown(object sender,MouseButtonEventArgs e)
+		{
+			e.Handled = true;
+
+            if ( e.ClickCount != 2 )
+            {
+                return;
+            }
+
+            var element = e.OriginalSource as FrameworkElement;
+
+            if ( element == null )
+            {
+                return;
+            }
+
+			var window = element.TemplatedParent as Window;
+
+            if ( window == null )
+            {
+                return;
+            }
+
+			if ( window.WindowState == System.Windows.WindowState.Normal)
+			{
+				window.MaxHeight    = SystemParameters.MaximizedPrimaryScreenHeight;
+				window.WindowState  = System.Windows.WindowState.Maximized;							
+			}
+			else
+			{
+				window.WindowState  = System.Windows.WindowState.Normal;
+			}
+		}
+
+        public void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+        {
+			var element = sender  as FrameworkElement;
+
+			if ( element == null )
+			{
+				return;
+			}
+
+			var window = element.TemplatedParent as Window;
+
+			if ( window == null )
+			{
+				return;
+			}
+
+			window.WindowState = System.Windows.WindowState.Minimized;
+        }
+
+		public void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+        {
+			var element = sender  as FrameworkElement;
+
+			if ( element == null )
+			{
+				return;
+			}
+
+			var window = element.TemplatedParent as Window;
+
+			if ( window == null )
+			{
+				return;
+			}
+
+			if (window.WindowState == System.Windows.WindowState.Normal)
+			{
+				window.MaxHeight   = SystemParameters.MaximizedPrimaryScreenHeight;
+				window.WindowState = System.Windows.WindowState.Maximized;
+			}
+			else
+			{
+				window.WindowState = System.Windows.WindowState.Normal;
+			}
+        }
+
+		public void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+			var element = sender  as FrameworkElement;
+
+			if ( element == null )
+			{
+				return;
+			}
+
+			var window = element.TemplatedParent as Window;
+
+			if ( window == null )
+			{
+				return;
+			}
+
+			window.Close();
+        }
+	}
+}
