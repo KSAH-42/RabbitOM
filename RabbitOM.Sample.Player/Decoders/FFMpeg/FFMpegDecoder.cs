3c38ef12014161da01feace3dc33dd3ae3654ce5
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-01 02:03:20 +0200

    reconfigure as single thread, there is no significant effect

--- RabbitOM.Sample.Player/Decoders/FFMpeg/FFMpegDecoder.cs
+++ RabbitOM.Sample.Player/Decoders/FFMpeg/FFMpegDecoder.cs
@@ -63,8 +63,8 @@ namespace RabbitOM.Sample.Player.Codecs.FFMpeg
                     throw new InvalidOperationException( "can not allocate a codec context" );
                 }
 
-	            _context->thread_count = Environment.ProcessorCount;
-                _context->thread_type = ffmpeg.FF_THREAD_FRAME | ffmpeg.FF_THREAD_SLICE;
+	            _context->thread_count = 1;
+                _context->flags  |= ffmpeg.AV_CODEC_FLAG_TRUNCATED;
                 _context->flags2 |= ffmpeg.AV_CODEC_FLAG2_FAST;
 
                 fixed( AVDictionary** opts = &_options )
