c677cd82e20d9f6e17db6c32236e26fdef1ab6d9
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-05 22:41:41 +0200

    modify the moveup and movedown method

--- RabbitOM.Player/Extensions/ObservableCollectionExtensions.cs
+++ RabbitOM.Player/Extensions/ObservableCollectionExtensions.cs
@@ -66,7 +66,7 @@ namespace RabbitOM.Player.Extensions
 
                 if ( index <= 0 )
                 {
-                    break;
+                    continue;
                 }
 
                 source.RemoveAt( index );
@@ -97,7 +97,7 @@ namespace RabbitOM.Player.Extensions
 
                 if ( index < 0 || source.Count - 1 <= index )
                 {
-                    break;
+                    continue;
                 }
 
                 source.RemoveAt( index );
