eb81833c77bd9a5140df8e8b3903270662769330
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-08 00:58:53 +0200

    adding the base interface for receiving data from udp, multicast, etc...

---
+++ RabbitOM.Experimentation/Rtspv2/Transports/IDataReceiver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.RtspV2.Transports
+{
+    // this interface will be used to receive data from UDP or Multicast
+    public interface IDataReceiver : IDisposable
+    {
+        bool IsOpened { get; }
+
+
+        void Open();
+
+        void Close();
+
+        int Receive( byte[] buffer , int offset , int count );
+    }
+}
