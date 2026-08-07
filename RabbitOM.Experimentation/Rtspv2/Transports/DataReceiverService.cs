e12ea1d7e688e87aa78e64a38a4c509cd417637f
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-08 01:17:48 +0200

    adding the data receiver class

---
+++ RabbitOM.Experimentation/Rtspv2/Transports/DataReceiverService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RabbitOM.Streaming.RtspV2.Transports
+{
+    public class DataReceiverService : IDisposable
+    {
+        private readonly IDataReceiver _receiver;
+
+
+        public DataReceiverService( IDataReceiver receiver )
+        {
+            _receiver = receiver ?? throw new ArgumentNullException( nameof( receiver ) );
+        }
+
+        ~DataReceiverService()
+        {
+            Dispose( false );
+        }
+
+        public bool IsStarted
+        {
+            get => throw new NotImplementedException( "To be implemented" );
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException( "To be implemented" );
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException( "To be implemented" );
+        }
+
+        public void Dispose()
+        {
+            Dispose( true );
+            GC.SuppressFinalize( this );
+        }
+
+        protected void Dispose( bool disposing )
+        {
+            if ( disposing )
+            {
+                Stop();
+            }
+        }
+    }
+}
