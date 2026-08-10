a15e500d79e83ec9ec3399a8f715b5d0720048de
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-10 23:45:19 +0200

    adding the configuration layer

--- RabbitOM.Player/Configuration/ApplicationConfiguration.cs
+++ RabbitOM.Player/Configuration/ApplicationConfiguration.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
+using System.Configuration;
+using System.Collections.Generic;
 
 namespace RabbitOM.Player.Configuration
 {
