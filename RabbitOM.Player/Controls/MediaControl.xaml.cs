3aa4670df7b1ad5b7b9d94be5179ce93e575fce8
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-12 12:16:37 +0200

    adding mediacontrol - to be implemented

---
+++ RabbitOM.Player/Controls/MediaControl.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RabbitOM.Player.Controls
+{
+    // to the replacement of the actual implementation located on the MainWindow class
+    public partial class MediaControl : UserControl
+    {
+        public MediaControl()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+
+        public static readonly RoutedEvent CommunicationStartedEvent = EventManager.RegisterRoutedEvent( "CommunicationStarted" , RoutingStrategy.Bubble , typeof(RoutedEventHandler) , typeof(MediaControl) );
+
+        public static readonly RoutedEvent CommunicationStoppedEvent = EventManager.RegisterRoutedEvent( "CommunicationStopped" , RoutingStrategy.Bubble , typeof(RoutedEventHandler) , typeof(MediaControl) );
+
+        public static readonly RoutedEvent ConnectedEvent = EventManager.RegisterRoutedEvent( "Connected" , RoutingStrategy.Bubble , typeof(RoutedEventHandler) , typeof(MediaControl) );
+
+        public static readonly RoutedEvent DisconnectedEvent = EventManager.RegisterRoutedEvent( "Disconnected" , RoutingStrategy.Bubble , typeof(RoutedEventHandler) , typeof(MediaControl) );
+
+
+
+
+
+
+
+
+
+
+        public event RoutedEventHandler CommunicationStarted
+        {
+            add    => AddHandler( CommunicationStartedEvent , value );
+            remove => RemoveHandler( CommunicationStartedEvent , value );
+        }
+
+        public event RoutedEventHandler CommunicationStopped
+        {
+            add    => AddHandler( CommunicationStoppedEvent , value );
+            remove => RemoveHandler( CommunicationStoppedEvent , value );
+        }
+
+        public event RoutedEventHandler Connected
+        {
+            add    => AddHandler( ConnectedEvent , value );
+            remove => RemoveHandler( ConnectedEvent , value );
+        }
+
+        public event RoutedEventHandler Disconnected
+        {
+            add    => AddHandler( DisconnectedEvent , value );
+            remove => RemoveHandler( DisconnectedEvent , value );
+        }
+
+
+
+
+
+
+
+
+
+
+        public bool IsCommunicationStarted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCommunication( string uri )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCommunicationWithUdp( string uri , int port )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCommunicationWithMulticast( string uri , string address , int port )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCommunicationWithMulticast( string uri , string address , int port , byte ttl )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopCommunication()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ImageSource GetImage()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+        private void OnLoaded( object sender , RoutedEventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUnloaded( object sender , RoutedEventArgs e )
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+        protected virtual void OnCommunicationStarted( RoutedEventArgs e )
+        {
+            RaiseEvent( new RoutedEventArgs( CommunicationStartedEvent , this ) );
+        }
+
+        protected virtual void OnCommunicationStopped( RoutedEventArgs e )
+        {
+            RaiseEvent( new RoutedEventArgs( CommunicationStoppedEvent , this ) );
+        }
+
+        protected virtual void OnConnected( RoutedEventArgs e )
+        {
+            RaiseEvent( new RoutedEventArgs( ConnectedEvent , this ) );
+        }
+
+        protected virtual void OnDisconnected( RoutedEventArgs e )
+        {
+            RaiseEvent( new RoutedEventArgs( DisconnectedEvent , this ) );
+        }
+    }
+}
---
+++ RabbitOM.Player/Controls/VisualWrapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Markup;
+
+namespace RabbitOM.Player.Controls
+{
+    [ContentProperty("Child")]
+    public class VisualWrapper : FrameworkElement
+    {
+        private Visual _child = null;
+
+        public Visual Child
+        {
+            get => _child;
+            set => SetupChild( ref _child , value );
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get => _child != null ? 1 : 0;
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            if ( index != 0 )
+            {
+                throw new ArgumentOutOfRangeException( nameof( index ) );
+            }
+
+            return _child ?? throw new InvalidOperationException();
+        }
+
+        private void SetupChild( ref Visual member, Visual child )
+        {
+            if ( member != null )
+            {
+                AddVisualChild( member );
+                AddLogicalChild( member );
+            }
+
+            member = child;
+
+            if ( member != null )
+            {
+                RemoveVisualChild( member );
+                RemoveLogicalChild( member );
+            }
+        }
+    }
+}
