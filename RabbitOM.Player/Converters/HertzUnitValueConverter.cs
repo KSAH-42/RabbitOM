b47de456d902270d6b1aaeac098a347dbf80b7a2
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-15 00:00:44 +0200

    adding unit hertz value converter

--- RabbitOM.Player/Converters/HertzUnitValueConverter.cs
+++ RabbitOM.Player/Converters/HertzUnitValueConverter.cs
@@ -7,31 +7,30 @@ using System.Linq;
 namespace RabbitOM.Player.Converters
 {
     [ValueConversion(typeof(byte), typeof(string))]
-    public sealed class MemorySizeValueConverter : IValueConverter
+    public sealed class HertzUnitValueConverter : IValueConverter
     {
         private readonly IReadOnlyList<string> FormatUnits = new List<string>()
         {
-            "{0:0.##} bytes",
-            "{0:0.##} Kbits",
-            "{0:0.##} Mbits",
-            "{0:0.##} Gbits",
-            "{0:0.##} Tbits"
+            "{0} Hz",
+            "{0} KHz",
+            "{0} MHz",
+            "{0} GHz",
         };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var size = System.Convert.ToDouble( value ) * 8;
-            var temp = (long) size;
+            var size = System.Convert.ToUInt64( value );
+            var temp = size;
             var index = 0;
 
-            while ( (temp /= 1024) > 0 )
+            while ( (temp /= 1000) > 0 )
             {
                 index ++;
             }
 
             var format = FormatUnits.ElementAtOrDefault( index ) ?? FormatUnits.LastOrDefault();
 
-            return string.Format( format , size / Math.Pow( 1024 , index ) );
+            return string.Format( format , size / Math.Pow( 1000 , index ) );
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
