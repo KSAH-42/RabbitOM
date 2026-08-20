0ff088f918b1c27e9d722a7052c2bd1a98d43966
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-21 01:15:31 +0200

    adding the command line parser

---
+++ RabbitOM.Player/Cli/CommandLineParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitOM.Player.Cli
+{
+    public static class CommandLineParser
+    {
+        public static bool TryParse( string[] args , out Command result )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
