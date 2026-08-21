727a74ed9c351fcf87cb83de3a93e154b4dabad4
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-21 11:03:24 +0200

    refactor

--- RabbitOM.Player/Cli/CommandLineParser.cs
+++ RabbitOM.Player/Cli/CommandLineParser.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace RabbitOM.Player.Cli
 {
     public static class CommandLineParser
     {
-        public static bool TryParse( string[] args , out Command result )
+        public static Command Parse( string[] args )
         {
             throw new NotImplementedException();
         }
