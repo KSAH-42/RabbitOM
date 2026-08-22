b12220e81c45a96ffb50aebbd9580b64c24b7bd3
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-22 09:13:37 +0200

    add the command line

---
+++ RabbitOM.Player/Cli/ICommandLine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RabbitOM.Player.Cli
+{
+    public interface ICommandLine : IDisposable
+    {
+        ICommandLine Register<TCommand>( Action<TCommand> action ) where TCommand : Command;
+
+        void Run( string[] args );
+    }
+}
