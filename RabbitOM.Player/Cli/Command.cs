dc27fe916840db014e1f3f2071c5ebfd5815bc47
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-22 09:13:14 +0200

    add IDisposable

--- RabbitOM.Player/Cli/Command.cs
+++ RabbitOM.Player/Cli/Command.cs
@@ -2,8 +2,23 @@
 
 namespace RabbitOM.Player.Cli
 {
-    public abstract class Command
+    public abstract class Command : IDisposable
     {
+        ~Command()
+        {
+            Dispose( false );
+        }
+
         public abstract void Execute();
+
+        public void Dispose()
+        {
+            Dispose( true );
+            GC.SuppressFinalize( this );
+        }
+
+        protected virtual void Dispose( bool disposing )
+        {
+        }
     }
 }
