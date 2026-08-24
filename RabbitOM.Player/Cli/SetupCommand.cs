3883d6d534cba068e5c4659e00f61b0aa5ae7cd5
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-24 05:16:21 +0200

    adding application interface

--- RabbitOM.Player/Cli/SetupCommand.cs
+++ RabbitOM.Player/Cli/SetupCommand.cs
@@ -5,6 +5,14 @@ namespace RabbitOM.Player.Cli
     [Command( "setup" )]
     public sealed class SetupCommand : Command
     {
+        private readonly IApplication _application;
+
+        public SetupCommand( IApplication application )
+        {
+            _application = application ?? throw new ArgumentNullException( nameof( application ) );
+        }
+
+
         [OptionPosition(0)]
         public string Uri { get; set; }
 
@@ -24,7 +32,30 @@ namespace RabbitOM.Player.Cli
         // RabbitOM.Player.exe setup rtsp://abcd --play --no-stats --no-stretch
         public override void Execute()
         {
-            throw new NotImplementedException();
+            if ( ShowStats )
+            {
+                _application.ShowStatistics();
+            }
+            else
+            {
+                _application.HideStatistics();
+            }
+
+            if ( StrechImage )
+            {
+                _application.StrechImage();
+            }
+            else
+            {
+                _application.UnStrechImage();
+            }
+
+            _application.StreamUri = Uri;
+
+            if ( AutoStart )
+            {
+                _application.StartStreaming();
+            }
         }
     }
 }
---
+++ RabbitOM.Player/IApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RabbitOM.Player
+{
+    public interface IApplication
+    {
+        string StreamUri { get; set; }
+
+        void ShowStatistics();
+
+        void HideStatistics();
+
+        void StrechImage();
+
+        void UnStrechImage();
+
+        void StartStreaming();
+
+        void StopStreaming();
+    }
+}
