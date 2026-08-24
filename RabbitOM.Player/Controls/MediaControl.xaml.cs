29f7408e5ba6da8cacfe4e725f722eb2379f2979
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-24 18:09:35 +0200

    continue the implementation of usercontrol

--- RabbitOM.Player/Controls/MediaControl.xaml.cs
+++ RabbitOM.Player/Controls/MediaControl.xaml.cs
@@ -1,19 +1,21 @@
-﻿// this component is used to display live stream based on rtsp uri
-// and... it does not used the state gof pattern, so why ?
-// normally a player has different states like: playing, paused, stopped, etc...
-// but here we have 2 states only: started and stopped. That's all.
-// it doesn"t really make sense to make a pause during a live
-// but not for replay yes. And this component is design for live streaming only
-// for instance, for the RtspV2, a RtspPlaybackReceiver must be used instead to control playback
-// RtspV2.Receivers has been introduce to used and to scale individual receivers most for live and the rest for playbacks
-// For the next implementation, and as mentionned the repository home page, the rtspclient will be replace by a receiver class
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace RabbitOM.Player.Controls
 {
+    using RabbitOM.Player.Codecs;
+    using RabbitOM.Player.Codecs.FFMpeg;
+    using RabbitOM.Streaming.Rtp;
+    using RabbitOM.Streaming.Rtp.H264;
+    using RabbitOM.Streaming.Rtp.H265;
+    using RabbitOM.Streaming.Rtp.Jpeg;
+    using RabbitOM.Streaming.Rtsp;
+    using RabbitOM.Streaming.Rtsp.Clients;
+
+    // TODO: finish to move the code from mainwindow to this usercontrol before modify the render
     public partial class MediaControl : UserControl
     {
         public static readonly RoutedEvent CommunicationStartedEvent = EventManager.RegisterRoutedEvent( "CommunicationStarted" , RoutingStrategy.Bubble , typeof(RoutedEventHandler) , typeof(MediaControl) );
@@ -22,15 +24,37 @@ namespace RabbitOM.Player.Controls
         public static readonly RoutedEvent DisconnectedEvent = EventManager.RegisterRoutedEvent( "Disconnected" , RoutingStrategy.Bubble , typeof(RoutedEventHandler) , typeof(MediaControl) );
         public static readonly RoutedEvent FrameReceivedEvent = EventManager.RegisterRoutedEvent( "Disconnected" , RoutingStrategy.Bubble , typeof(RoutedEventHandler) , typeof(MediaControl) );
 
+
+
+
         public static readonly DependencyProperty UriProperty = DependencyProperty.Register( "Uri", typeof(string) , typeof(MediaControl) );
         public static readonly DependencyProperty UserNameProperty = DependencyProperty.Register( "UserName", typeof(string) , typeof(MediaControl) );
         public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register( "Password", typeof(string) , typeof(MediaControl) );
         public static readonly DependencyProperty TransportProperty = DependencyProperty.Register( "Transport", typeof(MediaPlayerTransport) , typeof(MediaControl) );
+        public static readonly DependencyProperty StatusInfoProperty = DependencyProperty.Register( "StatusInfo", typeof(string) , typeof(MediaControl) );
+        public static readonly DependencyProperty FooterProperty = DependencyProperty.Register( "Footer", typeof(string) , typeof(MediaControl) );
+        public static readonly DependencyProperty FooterVisibilityProperty = DependencyProperty.Register( "FooterVisibility", typeof(Visibility) , typeof(MediaControl) , new PropertyMetadata( Visibility.Collapsed ) );
+
+
+
+
+        private readonly RtspClient _client = new RtspClient();
+        private readonly RtpPacketInspector _inspector = new DefaultRtpPacketInspector();
+        private readonly RtpMediaBuilderProxy _frameBuilder = new RtpMediaBuilderProxy();
+        private readonly Decoder _decoder = new FFMpegDecoder();
+        private readonly Renderer _renderer = new FFMpegRenderer();
+        private readonly NetworkStatisticsDataSource _datasource = new NetworkStatisticsDataSource();
+
+
+
 
         public MediaControl()
         {
             InitializeComponent();
         }
+
+
+
 
         public event RoutedEventHandler CommunicationStarted
         {
@@ -63,6 +87,8 @@ namespace RabbitOM.Player.Controls
         }
 
 
+
+
         public string Uri
         {
             get => GetValue( UriProperty ) as string;
@@ -87,50 +113,87 @@ namespace RabbitOM.Player.Controls
             set => SetValue( TransportProperty , value );
         }
 
+        public string StatusInfo
+        {
+            get => GetValue( StatusInfoProperty ) as string;
+            set => SetValue( StatusInfoProperty , value );
+        }
+
+        public string Footer
+        {
+            get => GetValue( FooterProperty ) as string;
+            set => SetValue( FooterProperty , value );
+        }
+
+        public NetworkStatistics Statistics
+        {
+            get => _statistics;
+        }
+
+
+
+
 
         public bool IsCommunicationStarted()
         {
-            throw new NotImplementedException();
+            return _client.IsCommunicationStarted;
         }
 
-        public void StartCommunication( string uri )
-        {
-            throw new NotImplementedException();
-        }
-
-        public void StartCommunicationWithUdp( string uri , int port )
-        {
-            throw new NotImplementedException();
-        }
-
-        public void StartCommunicationWithMulticast( string uri , string address , int port )
-        {
-            throw new NotImplementedException();
-        }
-
-        public void StartCommunicationWithMulticast( string uri , string address , int port , byte ttl )
+        public bool StartCommunication()
         {
             throw new NotImplementedException();
         }
 
         public void StopCommunication()
         {
+            if ( _client.IsCommunicationStarted )
+            {
+                _client.StopCommunication( TimeSpan.FromSeconds(2) );
+                _image.Source = null;
+            }
         }
 
         public ImageSource GetImage()
         {
-            throw new NotImplementedException();
+            return _image.Source;
         }
 
 
 
         private void OnLoaded( object sender , RoutedEventArgs e )
         {
+            _client.CommunicationStarted += OnCommunicationStarted;
+            _client.CommunicationStopped += OnCommunicationStopped;
+            _client.Connected += OnConnected;
+            _client.Disconnected += OnDisconnected;
+            _client.PacketReceived += OnPacketReceived;
+            _frameBuilder.MediaBuilded += OnBuildFrame;
+            _frameBuilder.PacketsLost += OnPacketsLost;
+            _decoder.Decoded += OnFrameDecoded;
+            _statistics.DataSource = _datasource;
+            _statistics.StartCollect();
         }
 
         private void OnUnloaded( object sender , RoutedEventArgs e )
         {
+            _statistics.StopMonitoring();
+            _statistics.DataSource = null;
+            _client.StopCommunication();
+            _client.CommunicationStarted -= OnCommunicationStarted;
+            _client.CommunicationStopped -= OnCommunicationStopped;
+            _client.Connected -= OnConnected;
+            _client.Disconnected -= OnDisconnected;
+            _client.PacketReceived -= OnPacketReceived;
+            _client.Dispose();
+            _frameBuilder.MediaBuilded -= OnBuildFrame;
+            _frameBuilder.PacketsLost -= OnPacketsLost;
+            _frameBuilder.Dispose();
+            _decoder.Decoded -= OnFrameDecoded;
+            _renderer.Dispose();
+            _decoder.Dispose();
         }
+
+
 
         protected virtual void OnCommunicationStarted()
         {
@@ -155,6 +218,159 @@ namespace RabbitOM.Player.Controls
         protected virtual void OnFrameReceived()
         {
             RaiseEvent( new RoutedEventArgs( FrameReceivedEvent , this ) );
+        }
+
+
+
+
+        private void OnCommunicationStarted( object sender , RtspClientCommunicationStartedEventArgs e )
+        {
+            Dispatcher.BeginInvoke( DispatcherPriority.Render , new Action( () =>
+            {
+                StatusInfo = "Connecting";
+            } ) );
+        }
+
+        private void OnCommunicationStopped( object sender , RtspClientCommunicationStoppedEventArgs e )
+        {
+            Dispatcher.BeginInvoke( DispatcherPriority.Render , new Action( () =>
+            {
+                _datasource.Clear();
+                StatusInfo = "";
+            } ) );
+        }
+
+        private void OnConnected( object sender , RtspClientConnectedEventArgs e )
+        {
+            Dispatcher.BeginInvoke( DispatcherPriority.Render , new Action( () =>
+            {
+                _frameBuilder.Dispose();
+
+                _datasource.SetConnectionStatusOn();
+                _datasource.SetTransport( _client.Configuration.DeliveryMode.ToString() );
+                _datasource.SetCodec( e.TrackInfo.Encoder );
+                _datasource.SetClock( e.TrackInfo.ClockRate );
+
+                try
+                {
+                    CodecType codec = FFMpegCodecTypeConverter.Convert( e.TrackInfo.Encoder );
+
+                    if ( codec == CodecType.Unknown )
+                    {
+                        StatusInfo = "Format not supported ( " + e.TrackInfo.Encoder + " )";
+                        return;
+                    }
+
+                    if ( codec == CodecType.H265 )
+                    {
+                        _frameBuilder.Setup( () => new H265FrameBuilder()
+                        {
+                            SPS = Convert.FromBase64String(e.TrackInfo.SPS) ,
+                            PPS = Convert.FromBase64String(e.TrackInfo.PPS) ,
+                            VPS = Convert.FromBase64String(e.TrackInfo.VPS) ,
+                        } );
+                    }
+
+                    if ( codec == CodecType.H264 )
+                    {
+                        _frameBuilder.Setup( () => new H264FrameBuilder()
+                        {
+                            SPS = Convert.FromBase64String(e.TrackInfo.SPS) ,
+                            PPS = Convert.FromBase64String(e.TrackInfo.PPS) ,
+                        } );
+                    }
+
+                    if ( codec == CodecType.MJPEG )
+                    {
+                        _frameBuilder.Setup( () => new JpegFrameBuilder() );
+                    }
+
+                    _decoder.Open( codec );
+                    _renderer.Open( _image );
+
+                    Footer = _client.Configuration.Uri;
+                    StatusInfo = "";
+                }
+                catch( Exception ex )
+                {
+                    StatusInfo = "Exception Error: " + ex.Message;
+                }
+            } ) );
+        }
+
+        private void OnDisconnected( object sender , RtspClientDisconnectedEventArgs e )
+        {
+            Dispatcher.BeginInvoke( DispatcherPriority.Render , new Action( () =>
+            {
+                StatusInfo = "Connecting - Communication Lost";
+                Footer = "";
+                _image.Source = null;
+
+                _datasource.SetConnectionStatusOff();
+                _frameBuilder.Clear();
+                _decoder.Close();
+                _renderer.Close();
+            } ));
+        }
+
+        private void OnPacketReceived( object sender , RtspPacketReceivedEventArgs e )
+        {
+            _datasource.AddBytesReceived( e.Packet.Data.Length );
+
+            // TODO: move the inspector when the rtp sequence will be completed, it should be used on OnBuildFrame function, but in reality the inspector must be used in before the decoder filter 
+
+            if ( RtpPacket.TryParse( e.Packet.Data , out var packet ) && _inspector.TryInspect( packet ) )
+            {
+                _frameBuilder.AddPacket( packet );
+
+                _datasource.IncreasePacketReceived();
+            }
+        }
+
+        private void OnPacketsLost( object sender , RtpPacketsLostEventArgs e )
+        {
+            _datasource.AddPacketsLost( e.NumberOfPacketLost );
+        }
+
+        private void OnBuildFrame( object sender , RtpMediaBuildedEventArgs e )
+        {
+            if ( ! _decoder.IsOpened )
+            {
+                return;
+            }
+
+            // TODO: add interface on the mediaelement for getting extraparameters and to avoid h264/h265 mediaelement cast
+
+            byte[] extraParameters = null;
+
+            if ( e.MediaElement is H265MediaElement h265Frame )
+            {
+                extraParameters = H265MediaElement.CreateExtraParameters( h265Frame );
+            }
+
+            else if ( e.MediaElement is H264MediaElement h264Frame )
+            {
+                extraParameters = H264MediaElement.CreateExtraParameters( h264Frame );
+            }
+
+            if ( ! _decoder.CanConfigure( extraParameters ) || _decoder.Configure( extraParameters ) )
+            {
+                _decoder.Decode( e.MediaElement.Buffer );
+            }
+        }
+
+        private void OnFrameDecoded( object sender , DecodedEventArgs e )
+        {
+            Dispatcher.BeginInvoke( DispatcherPriority.Render , new Action( () =>
+            {
+                using ( e.Surface ) // add this step for freeing unmanaged memory now
+                {
+                    _renderer.Render( e.Surface );
+
+                    _datasource.IncreaseFrameCount();
+                    _datasource.SetFrameSize( e.Surface.Height , e.Surface.Width );
+                }
+            }));
         }
     }
 }
