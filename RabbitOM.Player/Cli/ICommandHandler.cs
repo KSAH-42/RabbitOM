6f9fd735d577e77023a5f52fff6956f9af37f3f9
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-08-27 14:02:06 +0200

    removing comments

--- RabbitOM.Player/Cli/ICommandHandler.cs
+++ RabbitOM.Player/Cli/ICommandHandler.cs
@@ -1,7 +1,4 @@
-﻿// we remove the command.execute method in order to allow execute the same command with different code
-// for instance is to avoid to recreate the same command class but we need to execute a different task
-// so command handler will used here
-using System;
+﻿using System;
 
 namespace RabbitOM.Player.Cli
 {
