3aba6fac96fdf59711fc66e1698ca6e54a13e2ff
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-09-13 04:04:28 +0200

    continue the refactoring the media player

--- RabbitOM.Player/Controls/ViewPortControl.xaml.cs
+++ RabbitOM.Player/Controls/ViewPortControl.xaml.cs
@@ -4,9 +4,9 @@ using System.Windows.Controls;
 namespace RabbitOM.Player.Controls
 {
     // TODO: adding this class to project picture and add zoom caps and may be add videosource and launch something to call the update method and instanciated a visualhost for rendering in a seperate thread, we render actually into the main thread. and the service class could be injected here, it could be better to do that thing. but before, check if the control can be move as a custom control, but using event handler will add more complexity for people to maintain the class even it's feasible, usercontrol or customcontrol ? hmm constrol control could be the right way and the control is used by the mediaplayer control. and the viewport can include buffering. so implement as usercontrol and see what's happen next
-    public partial class ViewPort : UserControl
+    public partial class ViewPortControl : UserControl
     {
-        public ViewPort()
+        public ViewPortControl()
         {
             InitializeComponent();
         }
