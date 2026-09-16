9807eec72e3fe1aa7c6319a1ce5b2b4c08371849
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-09-16 09:59:34 +0200

    refactoring the viewport

--- RabbitOM.Player/Controls/ViewPortControl.xaml.cs
+++ RabbitOM.Player/Controls/ViewPortControl.xaml.cs
@@ -1,14 +1,153 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace RabbitOM.Player.Controls
 {
-    // TODO: adding this class to project picture and add zoom caps and may be add videosource and launch something to call the update method and instanciated a visualhost for rendering in a seperate thread, we render actually into the main thread. and the service class could be injected here, it could be better to do that thing. but before, check if the control can be move as a custom control, but using event handler will add more complexity for people to maintain the class even it's feasible, usercontrol or customcontrol ? hmm constrol control could be the right way and the control is used by the mediaplayer control. and the viewport can include buffering. so implement as usercontrol and see what's happen next
+    using RabbitOM.Player.Data;
+	using System.Windows.Input;
+
     public partial class ViewPortControl : UserControl
     {
+        public static readonly DependencyProperty StretchImageProperty = DependencyProperty.Register( nameof(StretchImage) , typeof(Stretch) , typeof(ViewPortControl) , new PropertyMetadata( Stretch.Fill ) );
+
         public ViewPortControl()
         {
             InitializeComponent();
+        }
+
+        public Stretch StretchImage
+        {
+            get => (Stretch) GetValue( StretchImageProperty );
+            set => SetValue( StretchImageProperty , value );
+        }
+
+        public ZoomProperties ZoomProperties { get; } = new ZoomProperties();
+
+        public Image Image { get => _image; }
+
+
+
+        public bool UpdateTransforms()
+        {
+            const double limit = 8;
+
+            if ( ! IsEnabled || ZoomProperties.SelectionWidth <= limit || ZoomProperties.SelectionHeight <= limit || ActualWidth <= limit || ActualHeight <= limit )
+            {
+                return false;
+            }
+
+            ZoomProperties.ScaleX = ActualWidth / InnerRectangle.ActualWidth;
+            ZoomProperties.ScaleY = ActualHeight / InnerRectangle.ActualHeight;
+            ZoomProperties.TranslationX = -ZoomProperties.SelectionInnerX * ZoomProperties.ScaleX;
+            ZoomProperties.TranslationY = -ZoomProperties.SelectionInnerY * ZoomProperties.ScaleY;
+            return true;
+        }
+
+        public void ClearZoomSelection()
+        {
+            ZoomProperties.ScaleX = 1.0;
+            ZoomProperties.ScaleY = 1.0;
+            ZoomProperties.TranslationX = 0;
+            ZoomProperties.TranslationY = 0;
+            ZoomProperties.SelectionInnerX = 0;
+            ZoomProperties.SelectionInnerY = 0;
+            ZoomProperties.SelectionX = 0;
+            ZoomProperties.SelectionY = 0;
+            ZoomProperties.SelectionWidth = 0;
+            ZoomProperties.SelectionHeight = 0;
+            ZoomProperties.TextPositionY = 0;
+            ZoomProperties.TextPositionX = 0;
+            InnerRectangle.Width = 0;
+            InnerRectangle.Height = 0;
+            OuterRectangle.Width = 0;
+            OuterRectangle.Height = 0;
+        }
+
+
+
+        private void OnControlEnabledChanged( object sender , DependencyPropertyChangedEventArgs e )
+        {
+            if ( ! IsEnabled )
+            {
+                ZoomProperties.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnCanvasMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var canvas = sender as Canvas;
+
+            if ( canvas == null )
+            {
+                return;
+            }
+
+            canvas.CaptureMouse();
+
+            ZoomProperties.StartPoint = e.GetPosition( canvas );
+
+            ClearZoomSelection();
+        }
+
+        private void OnCanvasMouseUp( object sender , MouseButtonEventArgs e )
+        {
+            var canvas = sender as Canvas;
+
+            if ( canvas == null )
+            {
+                return;
+            }
+
+            canvas.ReleaseMouseCapture();
+
+            UpdateTransforms();
+
+            ZoomProperties.Visibility = Visibility.Collapsed;
+        }
+
+        private void OnCanvasMouseMove(object sender, MouseEventArgs e)
+        {
+            if ( ! IsEnabled || e.LeftButton != MouseButtonState.Pressed )
+            {
+                return;
+            }
+
+            var canvas = sender as Canvas;
+
+            if ( canvas == null || ! canvas.IsMouseCaptured )
+            {
+                return;
+            }
+
+            var pos = e.GetPosition( canvas );
+
+            ZoomProperties.SelectionX = Math.Min(pos.X, ZoomProperties.StartPoint.X);
+            ZoomProperties.SelectionY = Math.Min(pos.Y, ZoomProperties.StartPoint.Y);
+            ZoomProperties.SelectionWidth = Math.Abs(pos.X - ZoomProperties.StartPoint.X);
+            ZoomProperties.SelectionHeight = Math.Abs(pos.Y - ZoomProperties.StartPoint.Y);
+
+            ZoomProperties.TextPositionX = ZoomProperties.SelectionX;
+            ZoomProperties.TextPositionY = ZoomProperties.SelectionY;
+
+            var min = Math.Min( ZoomProperties.SelectionWidth , ZoomProperties.SelectionHeight );
+
+            InnerRectangle.Width = min;
+            InnerRectangle.Height = min;
+
+            ZoomProperties.SelectionInnerX = ZoomProperties.SelectionX + ZoomProperties.SelectionWidth / 2 - min/2;
+            ZoomProperties.SelectionInnerY = ZoomProperties.SelectionY;
+
+            if ( ZoomProperties.SelectionWidth <= ZoomProperties.SelectionHeight )
+            {
+                ZoomProperties.SelectionInnerY += ZoomProperties.SelectionHeight / 2 - min/2;
+            }
+
+            Canvas.SetTop( InnerRectangle , ZoomProperties.SelectionInnerY );
+            Canvas.SetLeft( InnerRectangle , ZoomProperties.SelectionInnerX );
+
+            ZoomProperties.Visibility = Visibility.Visible;
         }
     }
 }
--- RabbitOM.Player/Data/ZoomProperties.cs
+++ RabbitOM.Player/Data/ZoomProperties.cs
@@ -22,6 +22,8 @@ namespace RabbitOM.Player.Data
         public static readonly DependencyProperty TextPositionYProperty = DependencyProperty.Register( nameof(TextPositionY), typeof(double), typeof(ZoomProperties));
         public static readonly DependencyProperty VisibilityProperty = DependencyProperty.Register( nameof(Visibility), typeof(Visibility), typeof(ZoomProperties), new PropertyMetadata( Visibility.Collapsed ));
 
+        public Point StartPoint { get; set; }
+
         public double ScaleX
         {
             get => (double) GetValue( ScaleXProperty );
