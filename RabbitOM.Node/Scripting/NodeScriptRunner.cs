68a082d26f7c850c9f66c050a7268d7c41a2879a
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-09-19 12:12:21 +0200

    adding the script runner

---
+++ RabbitOM.Node/Scripting/NodeScriptRunner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RabbitOM.Node.Scripting
+{
+	public interface IScriptRunner : IDisposable
+	{
+		bool IsStarted { get; }
+
+		void Start();
+
+		void Stop();
+
+		void PostEvent( object source , EventArgs e );
+	}
+}
