2b8e7e347555550ad98997830b994df837f298a5
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-09-22 02:30:29 +0200

    completed the O(n) refactoring

--- RabbitOM.Node/Application/ApplicationSettings.cs
+++ RabbitOM.Node/Application/ApplicationSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -6,7 +7,7 @@ namespace RabbitOM.Node.Application
 {
     public sealed class ApplicationSettings
     {
-        [Option( IsRequired = true , Help = "represent the rtsp uri" )]
+        [Option(IsRequired = true, Help = "represent the rtsp uri" )]
         public string Uri { get; private set; }
 
         [Option(Name = "-s", Help = "represent the file name of the script")]
@@ -28,6 +29,9 @@ namespace RabbitOM.Node.Application
 
 
 
+        // refactoring as O(n) see previous comit of the parse method
+        // TODO: add the support of IsRequired
+        // RabbiOM.Node https://www.youtube.com/watch?v=ENdpvW1otMQ&t=7196s --script spying@curie.xml --disable-logging
 
         public static ApplicationSettings Parse( string[] input )
         {
@@ -46,20 +50,48 @@ namespace RabbitOM.Node.Application
                 Uri = input[0],
             };
 
+            var properties = new Dictionary<string,PropertyInfo>();
 
-            // TODO: refactor as O(n) and move the code to a different class
-
-            for ( var i = 1 ; i < input.Length ; ++ i )
+            foreach ( var property in typeof( ApplicationSettings ).GetRuntimeProperties() )
             {
-                var property = typeof( ApplicationSettings )
-                    .GetRuntimeProperties()
-                    .Where( p => p.GetCustomAttributes<OptionAttribute>().Any( o => o.Name == input[i] ) )
-                    .FirstOrDefault()
-                    ;
+                properties[ property.Name ] = property;
+            }
 
-                if ( property != null )
+            var options = new List<OptionAttribute>();
+            var indexes = new Dictionary<int,PropertyInfo>();
+
+            // O(n+m) => O(n)
+            foreach ( var prop in properties.Values )
+            {
+                indexes[ options.Count ] = prop;
+                options.AddRange( prop.GetCustomAttributes<OptionAttribute>() );
+            }
+
+            var selectedProperties = new Dictionary<string,Tuple<PropertyInfo,OptionAttribute>>();
+            var items = input.ToHashSet();
+            var index = -1;
+
+            for ( var i = 0; i < options.Count; ++ i )
+            {
+                var option = options[ i ];
+
+                if ( indexes.ContainsKey( i ) )
                 {
-                    var option = property.GetCustomAttributes<OptionAttribute>().First( o => o.Name == input[i] );
+                    index = i;
+                }
+
+                if ( indexes.TryGetValue( index , out var property ) && items.Contains( option.Name ) )
+                {
+                    selectedProperties[ option.Name ] = new Tuple<PropertyInfo, OptionAttribute>( property , option );
+                }
+            }
+
+            index = -1;
+            while ( ++ index < input.Length )
+            {
+                if ( selectedProperties.TryGetValue( input[ index ] , out var selectedProperty ) )
+                {
+                    var (property,option) = selectedProperty;
 
                     if ( option.Value != null )
                     {
@@ -67,29 +99,94 @@ namespace RabbitOM.Node.Application
                     }
                     else
                     {
-                        var value = input.ElementAtOrDefault( ++ i ) ?? string.Empty;
+                        var value = input.ElementAtOrDefault( ++ index ) ?? string.Empty;
 
-                        if ( property.PropertyType == typeof( string ) )
+                        if ( value.StartsWith( "-" ) )
                         {
-                            property.SetValue( result , value );
+                            throw new InvalidOperationException( $"waiting a value without starting with a dash: {value}" );
                         }
-                        else if ( property.PropertyType == typeof( bool ) )
-                        {
-                            property.SetValue( result , bool.Parse( value ) );
-                        }
-                        else if ( property.PropertyType == typeof( int ) )
-                        {
-                            property.SetValue( result , int.Parse( value ) );
-                        }
-                        else if ( property.PropertyType == typeof( TimeSpan ) )
-                        {
-                            property.SetValue( result , TimeSpan.Parse( value ) );
-                        }
+
+                        property.SetValue( result , PropertyValueConverter.Convert( selectedProperty.Item1.PropertyType , value ) );
                     }
                 }
             }
 
             return result;
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public static ApplicationSettings Parse( string[] input )
+        //{
+        //    if ( input == null )
+        //    {
+        //        throw new ArgumentNullException( nameof( input ) , "no input is provided" );
+        //    }
+
+        //    if ( input.Length < 1 )
+        //    {
+        //        throw new ArgumentException( "no input is provided" , nameof( input ) );
+        //    }
+
+        //    var result = new ApplicationSettings()
+        //    {
+        //        Uri = input[0],
+        //    };
+
+
+        //    // TODO: refactor as O(n) and move the code to a different class
+
+        //    for ( var i = 1 ; i < input.Length ; ++ i )
+        //    {
+        //        var property = typeof( ApplicationSettings )
+        //            .GetRuntimeProperties()
+        //            .Where( p => p.GetCustomAttributes<OptionAttribute>().Any( o => o.Name == input[i] ) )
+        //            .FirstOrDefault()
+        //            ;
+
+        //        if ( property != null )
+        //        {
+        //            var option = property.GetCustomAttributes<OptionAttribute>().First( o => o.Name == input[i] );
+
+        //            if ( option.Value != null )
+        //            {
+        //                property.SetValue( result , option.Value );
+        //            }
+        //            else
+        //            {
+        //                var value = input.ElementAtOrDefault( ++ i ) ?? string.Empty;
+
+        //                if ( property.PropertyType == typeof( string ) )
+        //                {
+        //                    property.SetValue( result , value );
+        //                }
+        //                else if ( property.PropertyType == typeof( bool ) )
+        //                {
+        //                    property.SetValue( result , bool.Parse( value ) );
+        //                }
+        //                else if ( property.PropertyType == typeof( int ) )
+        //                {
+        //                    property.SetValue( result , int.Parse( value ) );
+        //                }
+        //                else if ( property.PropertyType == typeof( TimeSpan ) )
+        //                {
+        //                    property.SetValue( result , TimeSpan.Parse( value ) );
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return result;
+        //}
     }
 }
---
+++ RabbitOM.Node/Application/PropertyValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RabbitOM.Node.Application
+{
+    public static class PropertyValueConverter
+    {
+        public static object Convert( Type type , string value )
+        {
+            if ( type == typeof( string ) )
+            {
+                return value ?? string.Empty;
+            }
+            else if ( type == typeof( bool ) )
+            {
+                return bool.Parse( value );
+            }
+            else if ( type == typeof( int ) )
+            {
+                return int.Parse( value );
+            }
+            else if ( type == typeof( TimeSpan ) )
+            {
+                return TimeSpan.Parse( value );
+            }
+            else
+            {
+                throw new NotSupportedException( "DataType not supported" );
+            }
+        }
+    }
+}
