8f686194a4c44e770c54865e647ae0c52130f1ae
Kader SAHNINE <a.sahnine@netcourrier.com>
2026-09-26 10:56:31 +0200

    adding a button to visit the web site

--- RabbitOM.Player/Dialogs/AboutDialog.xaml.cs
+++ RabbitOM.Player/Dialogs/AboutDialog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
 
@@ -9,6 +10,7 @@ namespace RabbitOM.Player.Dialogs
 
     public partial class AboutDialog : Window
     {
+        public static readonly RoutedCommand VisiteWebSiteCommand = new RoutedCommand();
         public static readonly RoutedCommand CloseCommand = new RoutedCommand();
 
         public AboutDialog()
@@ -17,6 +19,23 @@ namespace RabbitOM.Player.Dialogs
         }
 
         public ReadOnlyObservableCollection<ModuleInfo> Modules { get; } = new ObservableCollection<ModuleInfo>( ModuleInfoFactory.GetCurrentProcessModules() ).ToReadOnly();
+
+        private void OnVisiteWebSite( object sender , ExecutedRoutedEventArgs e )
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo() { UseShellExecute = true , FileName = "https://github.com/KSAH-42/RabbitOM" }
+            };
+
+            try
+            {
+                process.Start();
+            }
+            catch ( Exception ex )
+            {
+                MessageBox.Show( ex.ToString() );
+            }
+        }
 
         private void OnClose( object sender , ExecutedRoutedEventArgs e )
         {
